b8cc80ac902c82f2dccb2e434533555420c5ff2c
Dennis Mikkelson <mikkelsond@uwstout.edu>
2000-07-10 22:04:44 +0000

    Not done...partly modified for JDK 1.2.2

--- anl/ipns/ViewTools/UI/SplitPaneWithState.java
+++ anl/ipns/ViewTools/UI/SplitPaneWithState.java
@@ -1,16 +1,28 @@
 package DataSetTools.components.containers;
 
+/**
+ * $Id$ 
+ *  
+ * $Log$
+ * Revision 1.2  2000/07/10 22:04:44  dennis
+ * Not done...partly modified for JDK 1.2.2
+ *
+ * Revision 1.3  2000/06/12 20:12:10  dennis
+ * internal class now implements Serializable
+ *
+ * Revision 1.2  2000/05/02 15:35:09  dennis
+ * *** empty log message ***
+ *
+ *
+ */
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
 
-/**
- *  
- *  
- */
 public class SplitPaneWithState extends    JSplitPane
-                                         implements Serializable
+                                           implements Serializable
  {
     private   float  divider_location = 0.5f;
 
@@ -44,7 +56,7 @@ public class SplitPaneWithState extends    JSplitPane
          return;
        if ( size.width <= 0 || size.height <=0 ) 
          return;
-
+/*
        int orientation   = getOrientation();
        int pix_location;
 
@@ -54,33 +66,17 @@ public class SplitPaneWithState extends    JSplitPane
          pix_location = (int)(divider_location * size.height);
 
        setDividerLocation( pix_location );
-      // repaint();
+*/
+       setDividerLocation( (double)divider_location );
     }
 
 
-   class ResizeHandler extends ComponentAdapter
+   class ResizeHandler extends    ComponentAdapter
+                       implements Serializable
    {
       public void componentResized( ComponentEvent c )
       {
-/*
-         int       pix_location  =  getDividerLocation();
-         Dimension size          =  getSize();
-         int       orientation   =  getOrientation();
-         float     new_location  =  0;
-
-         if ( orientation == JSplitPane.HORIZONTAL_SPLIT )
-           new_location = ((float)pix_location)/((float)size.width);
-         else
-           new_location = ((float)pix_location)/((float)size.height);
-
-         System.out.println("Resized called ....." + new_location);
-         if ( Math.abs( new_location - divider_location ) > 0.01 )
-         {
-           divider_location = new_location;
-           PositionDivider();
-         }
-*/
-           PositionDivider();
+        PositionDivider();
       }
    }
 
