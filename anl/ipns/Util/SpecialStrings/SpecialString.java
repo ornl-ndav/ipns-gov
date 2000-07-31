3f9eae6b04c489b6f729ae4ffac32fc4d0bc1769
Dennis Mikkelson <mikkelsond@uwstout.edu>
2000-07-31 15:39:27 +0000

    Added setString() string method to allow setting a new string value into the SpecialString object.

--- anl/ipns/Util/SpecialStrings/SpecialString.java
+++ anl/ipns/Util/SpecialStrings/SpecialString.java
@@ -2,6 +2,10 @@
  *  @(#)SpecialString.java  1999/07/15   Dennis Mikkelson
  *
  *  $Log$
+ *  Revision 1.4  2000/07/31 15:39:27  dennis
+ *  Added setString() string method to allow setting a new string value into
+ *  the SpecialString object.
+ *
  *  Revision 1.3  2000/07/10 22:55:37  dennis
  *  July 10, 2000 version... many changes
  *
@@ -36,12 +40,29 @@ public abstract class SpecialString  implements  Serializable
      * @param   message   The string that serves as the value of this 
      *                    SpecialString object.
      */
-
     public SpecialString( String message )
     {
 	this.message = message;
     }
 
+
+    /**
+     * Set the string value of a special string to the specified message.
+     *
+     * @param   message   The string that to be set as the value of this
+     *                    SpecialString object.
+     */
+    public void setString( String message )
+    {
+       this.message = message;
+    }
+
+    /**
+     * Get the String value of this SpecialString object
+     *
+     * @return The string value for this object
+     *
+     */
     public String toString()
     {
       return message;
