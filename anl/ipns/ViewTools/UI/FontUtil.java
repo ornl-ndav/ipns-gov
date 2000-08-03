0b01f4be3ab268c2e06775da5e7a9dc6f0d643c0
Dennis Mikkelson <mikkelsond@uwstout.edu>
2000-08-03 19:07:25 +0000

    Added MONO_FONT for mono spaced fonts in text areas

--- anl/ipns/ViewTools/UI/FontUtil.java
+++ anl/ipns/ViewTools/UI/FontUtil.java
@@ -3,6 +3,9 @@
  *
  * ---------------------------------------------------------------------------
  *  $Log$
+ *  Revision 1.2  2000/08/03 19:07:25  dennis
+ *  Added MONO_FONT for mono spaced fonts in text areas
+ *
  *  Revision 1.1  2000/07/10 22:52:01  dennis
  *  Standard fonts for labels and borders, etc.
  *
@@ -25,6 +28,7 @@ public class FontUtil
 {
   static public final Font LABEL_FONT  = new Font("SansSerif", Font.PLAIN, 10);
   static public final Font BORDER_FONT = new Font("SansSerif", Font.BOLD,   9);
+  static public final Font MONO_FONT   = new Font("Monospaced", Font.PLAIN,12);
 
   /**
    * Don't instantiate this class, just use the Fonts provided.
