32b40a984a87f962612a03b428cfbcba5124e05a
Dennis Mikkelson <mikkelsond@uwstout.edu>
2000-10-03 21:20:42 +0000

    Added SortOnX method to sort a list of points.

--- anl/ipns/Util/Numeric/arrayUtil.java
+++ anl/ipns/Util/Numeric/arrayUtil.java
@@ -2,6 +2,9 @@
  * @(#)arrayUtil.java  1999/01/10   Dennis Mikkelson
  *
  *  $Log$
+ *  Revision 1.7  2000/10/03 21:20:42  dennis
+ *  Added SortOnX method to sort a list of points.
+ *
  *  Revision 1.6  2000/08/01 20:54:33  dennis
  *  Changed return value to -1 for get_index_of() method in the case that
  *  the x value searched for is larger than the last entry in x_vals[]
@@ -215,6 +218,21 @@ public static float interpolate( float x_value, float x[], float y[] )
 
 
 
+/**
+ *  Sort an array of 2D points based on the x coordinates of the points.
+ * 
+ *  @param  points   The array of 2D points to sort.
+ *
+ *  @return interpolated y value at the specified x value
+ */
+public static void SortOnX( floatPoint2D points[] )
+{
+  Compare_floatPoint2D_X comp = new Compare_floatPoint2D_X();
+
+  java.util.Arrays.sort( points, comp );
+}
+
+
 
   /**
    *  Extract a sequence of elements from the given array to form a new
@@ -315,5 +333,16 @@ public static float interpolate( float x_value, float x[], float y[] )
     for ( int i = 0; i < new_test.length; i++ )
       System.out.println( new_test[i] );
 
+   floatPoint2D points[] = new floatPoint2D[5];
+   points[0] = new floatPoint2D( 7, 30 );
+   points[1] = new floatPoint2D( 4, 25 );
+   points[2] = new floatPoint2D( 9, 35 );
+   points[3] = new floatPoint2D( 1, 10 );
+   points[4] = new floatPoint2D( 4, 26 );
+
+   arrayUtil.SortOnX( points );
+   for ( int i = 0; i < 5; i++ )
+     System.out.println( points[i] );
   }
+
 }
