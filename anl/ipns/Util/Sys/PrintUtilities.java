2899a0b439fd43be18e93c7ffa247c973e4a34fc
Dennis Mikkelson <mikkelsond@uwstout.edu>
2001-02-09 21:47:34 +0000

    Major update of CVS files to "ISAW 1.0 beta"

---
+++ anl/ipns/Util/Sys/PrintUtilities.java
@@ -0,0 +1,63 @@
+package IsawGUI;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.print.*;
+
+public class PrintUtilities implements Printable {
+  private Component componentToBePrinted;
+
+  public static void printComponent(Component c) {
+    new PrintUtilities(c).print();
+  }
+  
+  public PrintUtilities(Component componentToBePrinted) {
+    this.componentToBePrinted = componentToBePrinted;
+  }
+  
+  public void print() {
+    PrinterJob printJob = PrinterJob.getPrinterJob();
+    printJob.setPrintable(this);
+    System.out.println("Inside printutilities");
+    if (printJob.printDialog())
+      try {
+        printJob.print();
+      } catch(PrinterException pe) {
+        System.out.println("Error printing: " + pe);
+      }
+  }
+
+  public int print(Graphics g, PageFormat pageFormat, int pageIndex) {
+    if (pageIndex > 0) {
+      return(NO_SUCH_PAGE);
+    } else {
+      Graphics2D g2d = (Graphics2D)g;
+
+System.out.println("Inside printutilities x nad y are "+pageFormat.getImageableX());
+System.out.println("Inside printutilities x nad y are "+pageFormat.getImageableY());
+
+      g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+      disableDoubleBuffering(componentToBePrinted);
+      componentToBePrinted.paint(g2d);
+      enableDoubleBuffering(componentToBePrinted);
+      return(PAGE_EXISTS);
+    }
+  }
+
+  /** The speed and quality of printing suffers dramatically if
+   *  any of the containers have double buffering turned on.
+   *  So this turns if off globally.
+   *  @see enableDoubleBuffering
+   */
+  public static void disableDoubleBuffering(Component c) {
+    RepaintManager currentManager = RepaintManager.currentManager(c);
+    currentManager.setDoubleBufferingEnabled(false);
+  }
+
+  /** Re-enables double buffering globally. */
+  
+  public static void enableDoubleBuffering(Component c) {
+    RepaintManager currentManager = RepaintManager.currentManager(c);
+    currentManager.setDoubleBufferingEnabled(true);
+  }
+}
