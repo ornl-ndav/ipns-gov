34b9ae3e09904a659c55b37253bdf38ee6677e7b
Dennis Mikkelson <mikkelsond@uwstout.edu>
2001-04-23 21:50:34 +0000

    Added copyright and GPL info at the start of the file.

--- anl/ipns/ViewTools/UI/TextRangeUI.java
+++ anl/ipns/ViewTools/UI/TextRangeUI.java
@@ -1,7 +1,34 @@
 /*
- * @(#)TextRangeUI.java
+ * File:  TextRangeUI.java
  *
- *  Programmer: Dennis Mikkelson
+ * Copyright (C) 2000, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
  *
  *  2000/04/28  1.01 Dennis Mikkelson
  *                   Minor refinements, use NumberFormat and setFont()
@@ -11,6 +38,9 @@
  *                   label 
  *
  *  $Log$
+ *  Revision 1.3  2001/04/23 21:50:29  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.2  2001/01/29 21:43:38  dennis
  *  Now uses CVS version numbers.
  *
