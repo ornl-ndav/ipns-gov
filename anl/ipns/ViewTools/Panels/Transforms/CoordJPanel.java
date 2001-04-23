3777070abadfd2d8f66363051e87d64ce6bcb58c
Dennis Mikkelson <mikkelsond@uwstout.edu>
2001-04-23 21:15:16 +0000

    Added copyright and GPL info at the start of the file.

--- anl/ipns/ViewTools/Panels/Transforms/CoordJPanel.java
+++ anl/ipns/ViewTools/Panels/Transforms/CoordJPanel.java
@@ -1,9 +1,39 @@
 /*
- * @(#)  CoordJPanel.java
+ * File:  CoordJPanel.java
  *
- *  Programmer: Dennis Mikkelson
+ * Copyright (C) 1999, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
  *
  *  $Log$
+ *  Revision 1.6  2001/04/23 21:14:55  dennis
+ *  Added copyright and GPL info at the start of the file.
+ *
  *  Revision 1.5  2001/03/30 19:21:10  dennis
  *  The method   setLocalWorldCoordinates( bounds )   now intersects "bounds"
  *  with the current global world coordinates to make sure that the local
@@ -24,7 +54,6 @@
  *  Added RCS logging
  *
  */
-
 
 package DataSetTools.components.image;
 
@@ -162,11 +191,6 @@ abstract public class CoordJPanel extends    JPanel
   }
 
 
-/* ------------------------- LocalTransformChanged ------------------------ */
-
-  abstract protected void LocalTransformChanged();
-
-
 /* ------------------------------ showState ------------------------------ */
 
 public void showState( String str )
@@ -302,6 +326,17 @@ public boolean isDoingBox()
 {
   return doing_box;
 }
+
+
+/* --------------------------------------------------------------------------
+ *
+ *  PROTECTED METHODS
+ *
+ */
+
+/* ------------------------- LocalTransformChanged ------------------------ */
+
+  abstract protected void LocalTransformChanged();
 
 
 /* -----------------------------------------------------------------------
