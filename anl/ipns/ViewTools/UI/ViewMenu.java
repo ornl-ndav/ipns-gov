9da9f99588f31801bf8d0060db0dcc70a8397dfb
neffk <neffk@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2001-07-25 18:20:57 +0000

    encapsulates a menu that displays and listens to various view options.

---
+++ anl/ipns/ViewTools/UI/ViewMenu.java
@@ -0,0 +1,98 @@
+
+/*
+ * $Id$
+ *
+ * $Log$
+ * Revision 1.1  2001/07/25 18:20:57  neffk
+ * encapsulates a menu that displays and listens to various view
+ * options.
+ *
+ */
+ 
+package DataSetTools.components.ui;
+
+import DataSetTools.dataset.DataSet;
+import DataSetTools.viewer.ViewManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+
+/**
+ * builds a menu of different options for viewing data.  by default,
+ * a simple listener is added to the menu.  to override this behavior,
+ * call the constructor that takes (in addition to other things) an
+ * ActionListener.
+ */
+public class ViewMenu
+{
+
+  private static final String VIEW_M             = "View";
+  private static final String IMAGE_VIEW_MI      = "Image View";
+  private static final String SCROLL_VIEW_MI     = "Scrolled Graph View";
+  private static final String SELECTED_VIEW_MI   = "Selected Graph View";
+  private static final String THREED_VIEW_MI     = "3D View";
+
+  private DataSet[]  dss = null;
+
+
+  /**
+   * builds a menu of each view option.  this constructor uses the default
+   * ActionListener.
+   */ 
+  public void build( JMenu main_menu, 
+                     DataSet[] dss )
+  {
+    build( main_menu, dss, new DefaultViewMenuListener() );
+  }
+
+
+  public void build( JMenu main_menu,
+                     DataSet[] dss,
+                     ActionListener listener )
+  {
+    this.dss = dss;
+
+    JMenuItem image_mi  = new JMenuItem( IMAGE_VIEW_MI );
+    JMenuItem scroll_mi = new JMenuItem( SCROLL_VIEW_MI );
+    JMenuItem select_mi = new JMenuItem( SELECTED_VIEW_MI );  
+    JMenuItem threed_mi = new JMenuItem( THREED_VIEW_MI );
+
+    main_menu.add( image_mi );
+    main_menu.add( scroll_mi );
+    main_menu.add( select_mi );
+    main_menu.add( threed_mi );
+
+    main_menu.addActionListener( listener );
+  }
+
+
+  /* 
+   * listens to this menu and provides default actions for each
+   * menu item.  note that this class is NOT a container.  all
+   * containing is done by ViewMenu, which is responsible for
+   * the details of knowing which DataSet objects to show.
+   */
+  class DefaultViewMenuListener implements ActionListener
+  {
+    public void actionPerformed( ActionEvent e )
+    {
+      System.out.println( "ViewMenu option selected" ); 
+
+      if(  e.getActionCommand().equals( ViewManager.IMAGE )  )
+      {
+      }
+      if(  e.getActionCommand().equals( ViewManager.SCROLLED_GRAPHS )  )
+      {
+      }
+      if(  e.getActionCommand().equals( ViewManager.SELECTED_GRAPHS )   )
+      {
+      }
+      if(  e.getActionCommand().equals( ViewManager.THREE_D )  )
+      {
+      }
+    }
+  }
+
+}
