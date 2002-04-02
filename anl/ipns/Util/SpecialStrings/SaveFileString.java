ffdf1cda0390304e0882c8a3d8cb51ad9319a984
pfpeterson <petersonpf@ornl.gov>
2002-04-02 22:50:02 +0000

    Added to CVS.

---
+++ anl/ipns/Util/SpecialStrings/SaveFileString.java
@@ -0,0 +1,58 @@
+/*
+ * File:  SaveFileString.java
+ *
+ * Copyright (C) 2002, Peter Peterson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Peter Peterson <pfpeterson@anl.gov>
+ *           Intense Pulsed Neutron Source
+ *           Argonne National Laboratory
+ *           Argonne, IL 60439-4845
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.1  2002/04/02 22:50:02  pfpeterson
+ *  Added to CVS.
+ *
+ */
+package DataSetTools.util;
+
+import java.io.*;
+
+/**
+ * The SaveFileString class is used to pass a filename string between
+ * operators and the GUI so that appropriate GUI components can be
+ * created to get the input values from the user. This is used
+ * exclusively to select existing files.
+ */
+public class SaveFileString  extends     SpecialString{
+    public SaveFileString( ){
+        super("");
+    }
+    
+    
+    public SaveFileString( String message ){
+        super( message );
+    }
+    
+}
