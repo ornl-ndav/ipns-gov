63ad8597a6632fd55240efe979e2a7751bd9c49a
Dennis Mikkelson <mikkelsond@uwstout.edu>
2002-04-11 20:56:47 +0000

    Interface for functions of one variable that are controlled by an array of parameters and that return float or double values when evaluated at single points, or at arrays of points.

---
+++ anl/ipns/MathTools/Functions/IOneVarParameterizedFunction.java
@@ -0,0 +1,61 @@
+/**
+ * File: IOneVarParameterizedFunction.java
+ *
+ * Copyright (C) 2002, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.1  2002/04/11 20:56:47  dennis
+ *  Interface for functions of one variable that are controlled by
+ *  an array of parameters and that return float or double
+ *  values when evaluated at single points, or at arrays of points.
+ *
+ *  Revision 1.1  2002/04/04 19:44:23  dennis
+ *  Interface for functions of one variable that are controlled
+ *  by a list of parameters.
+ *
+ */
+
+package DataSetTools.functions;
+
+import DataSetTools.math.*;
+import DataSetTools.util.*;
+
+/**
+ *  This interface is the interface for functions of one variable that
+ *  also depend on an array of parameters.  The parameters are named. 
+ */
+
+public interface IOneVarParameterizedFunction extends IOneVarFunction
+{
+  public int      numParameters(); 
+  public float[]  getParameters();
+  public void     setParameters( float parameters[] );
+  public String[] getParameterNames();
+}
