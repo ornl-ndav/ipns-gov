90b44a9c66d8bde8b3749854e84dae3498a257b1
Dennis Mikkelson <mikkelsond@uwstout.edu>
2002-04-11 20:55:00 +0000

    Interface for functions of one variable that return float or double values when evaluated at single points, or at arrays of points.

---
+++ anl/ipns/MathTools/Functions/IOneVarFunction.java
@@ -0,0 +1,67 @@
+/**
+ * File: IOneVarFunction.java
+ *
+ * Copyright (C) 2002, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI. 54751
+ *           USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log$
+ *  Revision 1.1  2002/04/11 20:55:00  dennis
+ *  Interface for functions of one variable that return float or double
+ *  values when evaluated at single points, or at arrays of points.
+ *
+ *  Revision 1.1  2002/04/04 19:42:41  dennis
+ *  Interface for functions of one variable.
+ *
+ */
+
+package DataSetTools.functions;
+
+import DataSetTools.math.*;
+import DataSetTools.util.*;
+
+/**
+ *  This interface is the interface for Named functions of one variable that
+ *  can produce float or double values at one point or an array of points.
+ *  These functions are assumed to be zero outside of a closed interval
+ *  that is their domain.
+ */
+public interface IOneVarFunction extends IOneVariableFunction
+{
+  public double   getValue( double x );
+  public float[]  getValues( float x[] );
+
+  public float    getValue( float x );
+  public double[] getValues( double x[] );
+
+  public String   getName(); 
+  public void     setName( String name ); 
+
+  public ClosedInterval getDomain(); 
+  public void setDomain( ClosedInterval interval ); 
+}
