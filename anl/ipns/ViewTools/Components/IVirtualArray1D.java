85d31ed5a5e38003b1282c62e29b89de0bdf27ea
serumb <serumb@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2003-06-06 18:33:57 +0000

    initial version of an interface for a VirtualArray1D

---
+++ anl/ipns/ViewTools/Components/IVirtualArray1D.java
@@ -0,0 +1,150 @@
+/*
+ * File: IVirtualArray1D.java
+ *
+ * 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * 
+ *           
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0218882, and by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ *
+ *  $Log: 
+ *
+ */
+ 
+package DataSetTools.components.View;
+
+//import DataSetTools.components.View.OneD.*;
+ import javax.swing.*;
+ import java.awt.event.*;
+/**
+ * This interface is implemented by classes that can produce a virtual function 
+ * list containing a 3Darray of floats and is used to pass data to viewers and 
+ * view components.Along with the data, some data attributes are kept in the 
+ * virtual FunctionList. 
+ * the x dimension is either 0 or 1. 0 for the x values 1 for the y values
+ * the y dimension contains a number of x and y values 
+ * the z dimension is represents the line numbers.
+
+ */
+
+public interface IVirtualArray1D
+{
+   public static final String NO_XLABEL = "No X Label";
+   public static final String NO_YLABEL = "No Y Label";
+   public static final String NO_TITLE  = "No Title";
+   public static final String NO_XUNITS = "No X Units";
+   public static final String NO_YUNITS = "No X Units";
+  /**
+   * Returns the attributes of the data array in a AxisInfo2D wrapper.
+   * This method will take in a boolean value to determine for which axis
+   * info is being retrieved for.    true = X axis, false = Y axis.
+   */
+  
+   
+  /**
+   * This method will return the title assigned to the data. 
+   */
+   public String getTitle();
+   
+  /**
+   * This method will assign a title to the data. 
+   */
+   public void setTitle( String title );
+  
+  /*
+   ***************************************************************************
+   * The following methods must include implementation to prevent
+   * the user from exceeding the initial array size determined
+   * at creation of the array. If an M x N array is specified,
+   * the parameters must not exceed (M-1,N-1). 
+   ***************************************************************************
+   */
+   
+  /**
+   * Get values for a portion or all of a row.
+   * The "from" and "to" values must be direct array reference, i.e.
+   * because the array positions start at zero, not one, this must be
+   * accounted for. If the array passed in exceeds the bounds of the array, 
+   * get values for array elements and ignore extra values.
+   */
+   public float[] getXValues( int line_number );
+   
+  /**
+   * Set values for a portion or all of a row.
+   * The "from" and "to" values must be direct array reference, i.e.
+   * because the array positions start at zero, not one, this must be
+   * accounted for. If the array passed in exceeds the bounds of the array, 
+   * set values for array elements and ignore extra values.
+   */
+   public void setXValues( float[] values ,int line_num);
+   
+  /**
+   * Get values for a portion or all of a column.
+   * The "from" and "to" values must be direct array reference, i.e.
+   * because the array positions start at zero, not one, this must be
+   * accounted for. If the array passed in exceeds the bounds of the array, 
+   * get values for array elements and ignore extra values.
+   */
+   public float[] getYValues( int line_number );
+   
+  /**
+   * Set values for a portion or all of a column.
+   * The "from" and "to" values must be direct array reference, i.e.
+   * because the array positions start at zero, not one, this must be
+   * accounted for. If the array passed in exceeds the bounds of the array, 
+   * set values for array elements and ignore extra values.
+   */
+   public void setYValues( float[] values, int line_num);
+   
+
+   //**/get axisinfo 2d
+   public AxisInfo2D  getAxisInfo( boolean x_axis);
+
+
+     
+  /**
+   * Set all values in the array to a value. This method will usually
+   * serve to "initialize" or zero out the array. 
+   */
+   public void setAllValues( float value );
+
+   public int getNumPoints( int line_number );
+
+  /**
+   * Returns number of lines in the array.
+   */   
+   public int getNumlines();
+
+   public JComponent[] getSharedControls();
+   public JComponent[] getPrivateControls();
+   
+   public void addActionListener( ActionListener listener);
+   public void removeActionListener( ActionListener listener);
+   public void removeAllActionListeners();
+   
+}
