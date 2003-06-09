4c4f705ed3f954df2e882f7f14d410e8dec958a4
Dennis Mikkelson <mikkelsond@uwstout.edu>
2003-06-09 22:34:31 +0000

    - Added methods help() and getFocus()  (Mike Miller)

--- anl/ipns/ViewTools/Components/Transparency/IOverlay.java
+++ anl/ipns/ViewTools/Components/Transparency/IOverlay.java
@@ -34,6 +34,9 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.3  2003/06/09 22:34:31  dennis
+ *  - Added methods help() and getFocus()  (Mike Miller)
+ *
  *  Revision 1.2  2003/05/16 14:57:09  dennis
  *  Added acknowledgement of NSF funding.
  *
@@ -43,7 +46,23 @@ package DataSetTools.components.View.Transparency;
 
 import java.awt.*;
 
+/**
+ * This interface lays out methods to be implemented by all view component
+ * overlays.
+ */
 public interface IOverlay
 {
+  /**
+   * This method repaints the overlay. In most instances, this method will be
+   * implemented by the OverlayJPanel via the JPanel implementation.
+   */
    public void repaint();
+  
+  /**
+   * This method will give the focus of the overlay. In many cases the overlay
+   * will need one of it's private data members to be granted window focus.
+   * getFocus() will allow the overlay to specify focus by using requestFocus()
+   * within this method.
+   */ 
+   public void getFocus();
 }
