f4d46d8a6adadc2f7a03a31b70fae0425f0abc0a
millermi <millermi@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2003-10-22 20:06:23 +0000

    - Added java docs.

--- anl/ipns/ViewTools/Components/AxisInfo2D.java
+++ anl/ipns/ViewTools/Components/AxisInfo2D.java
@@ -34,6 +34,9 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.4  2003/10/22 20:06:23  millermi
+ *  - Added java docs.
+ *
  *  Revision 1.3  2003/08/07 15:57:43  dennis
  *  - Added copy() method to make a copy of an AxisInfo2D object.
  *    (Mike Miller)
@@ -45,103 +48,110 @@
 
 package DataSetTools.components.View;
 
-import java.awt.*;
-import java.lang.*;
-
 /**
  * This class groups all of the information about an axis into one object.
  * The class contains the "world coordinates" minimum and maximum, the
  * label, and units. This simple class has no set methods, 
  * all info must be set by the constructor.
+ *
+ *  @see DataSetTools.components.View.IVirtualArray2D
  */
-
 public class AxisInfo2D
 {  
-   public static final boolean XAXIS = true;
-   public static final boolean YAXIS = false;
+ /**
+  * true - this boolean variable defines the use of the x axis.
+  */ 
+  public static final boolean XAXIS = true;
+   
+ /**
+  * false - this boolean variable defines the use of the y axis.
+  */ 
+  public static final boolean YAXIS = false;
+   
+  private float axismin;
+  private float axismax; 
+  private String axislabel;
+  private String axisunits;  
+  private boolean islinear;
+  
+ /**
+  * Constructor: initializes axis information
+  *
+  *  @param  min
+  *  @param  max
+  *  @param  label
+  *  @param  units
+  *  @param  bool_lin
+  */
+  public AxisInfo2D( float min, float max, String label, 
+		     String units, boolean bool_lin )
+  {	 
+    axismin = min;
+    axismax = max;
+    axislabel = label;
+    axisunits = units;
+    islinear = bool_lin;
+  }
+  
+ /**
+  * Returns the minimum value of the axis.
+  *
+  *  @return the minumum value of the axis
+  */ 
+  public float getMin()
+  {
+    return axismin;
+  }
+ 
+ /**
+  * Returns the maximum value of the axis.
+  *
+  *  @return the maximum value of the axis
+  */ 
+  public float getMax()
+  {
+    return axismax;
+  }
     
-   private float axismin;
-   private float axismax; 
-   private String axislabel;
-   private String axisunits;  
-   private boolean islinear;
-   
-  /**
-   * Constructor: initializes axis information
-   *
-   *  @param  min
-   *  @param  max
-   *  @param  label
-   *  @param  units
-   *  @param  bool_lin
-   */
-   public AxisInfo2D( float min, float max, String label, 
-                      String units, boolean bool_lin )
-   {      
-      axismin = min;
-      axismax = max;
-      axislabel = label;
-      axisunits = units;
-      islinear = bool_lin;
-   }
-   
-  /**
-   * Returns the minimum value of the axis.
-   *
-   *  @return the minumum value of the axis
-   */ 
-   public float getMin()
-   {
-      return axismin;
-   }
+ /**
+  * Returns the label of the axis.
+  *
+  *  @return the label of the axis
+  */
+  public String getLabel()
+  {
+    return axislabel;
+  }
+ 
+ /**
+  * Returns the units of the axis.
+  *
+  *  @return the units of the axis
+  */ 
+  public String getUnits()
+  {
+    return axisunits;
+  }
   
-  /**
-   * Returns the maximum value of the axis.
-   *
-   *  @return the maximum value of the axis
-   */ 
-   public float getMax()
-   {
-      return axismax;
-   }
-     
-  /**
-   * Returns the label of the axis.
-   *
-   *  @return the label of the axis
-   */
-   public String getLabel()
-   {
-      return axislabel;
-   }
+ /**
+  * Returns boolean answer to question "Is Linear?". This value
+  * will be true if the calibration for the axis is linear. 
+  *
+  *  @return true if linear, false if logarithmic
+  */
+  public boolean getIsLinear()
+  {
+    return islinear;
+  } 
   
-  /**
-   * Returns the units of the axis.
-   *
-   *  @return the units of the axis
-   */ 
-   public String getUnits()
-   {
-      return axisunits;
-   }
-   
-  /**
-   * Returns boolean answer to question "Is Linear?". This value
-   * will be true if the calibration for the axis is linear. 
-   *
-   *  @return true if linear, false if logarithmic
-   */
-   public boolean getIsLinear()
-   {
-      return islinear;
-   } 
-   
-  /**
-   * This method returns a copy of the AxisInfo2D object.
-   */
-   public AxisInfo2D copy()
-   {
-     return new AxisInfo2D( axismin, axismax, new String(axislabel), 
-                            new String(axisunits), islinear );
-   }
+ /**
+  * This method returns a copy of the AxisInfo2D object.
+  *
+  *  @return copy of the AxisInfo2D object.
+  */
+  public AxisInfo2D copy()
+  {
+    return new AxisInfo2D( axismin, axismax, new String(axislabel), 
+			   new String(axisunits), islinear );
+  }
 }
