8c61b9087aa6a9d9feee3f9f79e6618475a98add
millermi <millermi@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2003-10-22 20:15:08 +0000

    - Removed methods now defined by IVirtualArray. - Added java docs where needed.

--- anl/ipns/ViewTools/Components/IVirtualArray2D.java
+++ anl/ipns/ViewTools/Components/IVirtualArray2D.java
@@ -34,6 +34,10 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.4  2003/10/22 20:15:08  millermi
+ *  - Removed methods now defined by IVirtualArray.
+ *  - Added java docs where needed.
+ *
  *  Revision 1.3  2003/08/07 15:56:14  dennis
  *  - Added method setAxisInfoVA() with alternate parameters.
  *    Since getAxisInfoVA() returns an AxisInfo2D object, this
@@ -67,133 +71,175 @@ import DataSetTools.components.View.TwoD.*;
  * M x N array has M rows, and N columns. The row numbers
  * start at zero and go to M-1 and the column numbers
  * start at zero and go to N-1. 
+ *
+ *  @see DataSetTools.components.View.AxisInfo2D
  */
 
-public interface IVirtualArray2D
+public interface IVirtualArray2D extends IVirtualArray
 {
-   public static final String NO_XLABEL = "No X Label";
-   public static final String NO_YLABEL = "No Y Label";
-   public static final String NO_TITLE  = "No Title";
-   public static final String NO_XUNITS = "No X Units";
-   public static final String NO_YUNITS = "No X Units";
-  /**
-   * Returns the attributes of the data array in a AxisInfo2D wrapper.
-   * This method will take in a boolean value to determine for which axis
-   * info is being retrieved for.    true = X axis, false = Y axis.
-   */
-   public AxisInfo2D getAxisInfoVA( boolean isX );
-   
-  /**
-   * Sets the attributes of the data array within a AxisInfo2D wrapper.
-   * This method will take in a boolean value to determine for which axis
-   * info is being altered.          true = X axis, false = Y axis.
-   */
-   public void setAxisInfoVA( boolean isX, float min, float max,
-                              String label, String units, boolean islinear ); 
-   
-  /**
-   * Sets the attributes of the data array within a AxisInfo2D wrapper.
-   * This method will take in a boolean value to determine for which axis
-   * info is being altered.          true = X axis, false = Y axis.
-   */
-   public void setAxisInfoVA( boolean isX, AxisInfo2D info );
-     
-  /**
-   * This method will return the title assigned to the data. 
-   */
-   public String getTitle();
-   
-  /**
-   * This method will assign a title to the data. 
-   */
-   public void setTitle( String title );
+ /**
+  * Returns the attributes of the data array in a AxisInfo2D wrapper.
+  * This method will take in a boolean value to determine for which axis
+  * info is being retrieved for.    true = X axis, false = Y axis.
+  *
+  *  @param  isX - Use AxisInfo2D.XAXIS (true) or AxisInfo2D.YAXIS (false). 
+  *  @return the axis info for the axis specified.
+  */
+  public AxisInfo2D getAxisInfoVA( boolean isX );
   
-  /*
-   ***************************************************************************
-   * The following methods must include implementation to prevent
-   * the user from exceeding the initial array size determined
-   * at creation of the array. If an M x N array is specified,
-   * the parameters must not exceed (M-1,N-1). 
-   ***************************************************************************
-   */
-   
-  /**
-   * Get values for a portion or all of a row.
-   * The "from" and "to" values must be direct array reference, i.e.
-   * because the array positions start at zero, not one, this must be
-   * accounted for. If the array passed in exceeds the bounds of the array, 
-   * get values for array elements and ignore extra values.
-   */
-   public float[] getRowValues( int row_number, int from, int to );
-   
-  /**
-   * Set values for a portion or all of a row.
-   * The "from" and "to" values must be direct array reference, i.e.
-   * because the array positions start at zero, not one, this must be
-   * accounted for. If the array passed in exceeds the bounds of the array, 
-   * set values for array elements and ignore extra values.
-   */
-   public void setRowValues( float[] values, int row_number, int start );
-   
-  /**
-   * Get values for a portion or all of a column.
-   * The "from" and "to" values must be direct array reference, i.e.
-   * because the array positions start at zero, not one, this must be
-   * accounted for. If the array passed in exceeds the bounds of the array, 
-   * get values for array elements and ignore extra values.
-   */
-   public float[] getColumnValues( int column_number, int from, int to );
-   
-  /**
-   * Set values for a portion or all of a column.
-   * The "from" and "to" values must be direct array reference, i.e.
-   * because the array positions start at zero, not one, this must be
-   * accounted for. If the array passed in exceeds the bounds of the array, 
-   * set values for array elements and ignore extra values.
-   */
-   public void setColumnValues( float[] values, int column_number, int start );
-   
-  /**
-   * Get value for a single array element.
-   */
-   public float getDataValue( int row_number, int column_number );
-   
-  /**
-   * Set value for a single array element.
-   */
-   public void setDataValue( int row_number, int column_number, float value );
-      
-  /**
-   * Set all values in the array to a value. This method will usually
-   * serve to "initialize" or zero out the array. 
-   */
-   public void setAllValues( float value );
-   
-  /**
-   * Returns the values in the specified region.
-   * The vertical dimensions of the region are specified by starting 
-   * at first row and ending at the last row. The horizontal dimensions 
-   * are determined by the first column and last column. 
-   */ 
-   public float[][] getRegionValues( int first_row, int last_row,
-                                     int first_column, int last_column );
-  /**  
-   * Sets values for a specified rectangular region. This method takes 
-   * in a 2D array that is already organized into rows and columns
-   * corresponding to a portion of the virtual array that will be altered.
-   */
-   public void setRegionValues( float[][] values, 
-                                int row_number,
-				int column_number );
-				
-  /**
-   * Returns number of rows in the array.
-   */
-   public int getNumRows();
+ /**
+  * Sets the attributes of the data array within a AxisInfo2D wrapper.
+  * This method will take in a boolean value to determine for which axis
+  * info is being altered.	    true = X axis, false = Y axis.
+  *
+  *  @param  isX - Use AxisInfo2D.XAXIS (true) or AxisInfo2D.YAXIS (false).
+  *  @param  min - Minimum value for this axis.
+  *  @param  max - Maximum value for this axis.
+  *  @param  label - label associated with the axis.
+  *  @param  units - units associated with the values for this axis.
+  *  @param  islinear - is axis linear (true) or logarithmic (false)
+  */
+  public void setAxisInfoVA( boolean isX, float min, float max,
+			     String label, String units, boolean islinear ); 
+  
+ /**
+  * Sets the attributes of the data array within a AxisInfo2D wrapper.
+  * This method will take in a boolean value to determine for which axis
+  * info is being altered.	    true = X axis, false = Y axis.
+  * 
+  *  @param  isX - Use AxisInfo2D.XAXIS (true) or AxisInfo2D.YAXIS (false).
+  *  @param  info - The axis info object associated with the axis specified.
+  */
+  public void setAxisInfoVA( boolean isX, AxisInfo2D info );
+  
+ /*
+  ***************************************************************************
+  * The following methods must include implementation to prevent
+  * the user from exceeding the initial array size determined
+  * at creation of the array. If an M x N array is specified,
+  * the parameters must not exceed (M-1,N-1). 
+  ***************************************************************************
+  */
+  
+ /**
+  * Get values for a portion or all of a row.
+  * The "from" and "to" values must be direct array reference, i.e.
+  * because the array positions start at zero, not one, this must be
+  * accounted for. If the array passed in exceeds the bounds of the array, 
+  * get values for array elements and ignore extra values.
+  *
+  *  @param  row   the row number being altered
+  *  @param  from  the column number of first element to be altered
+  *  @param  to    the column number of the last element to be altered
+  *  @return If row, from, and to are valid, an array of floats containing
+  *	     the specified section of the row is returned.
+  *	     If row, from, or to are invalid, an empty 1-D array is returned.
+  */
+  public float[] getRowValues( int row, int from, int to );
+  
+ /**
+  * Set values for a portion or all of a row.
+  * The "from" and "to" values must be direct array reference, i.e.
+  * because the array positions start at zero, not one, this must be
+  * accounted for. If the array passed in exceeds the bounds of the array, 
+  * set values for array elements and ignore extra values.
+  *
+  *  @param values  array of elements to be put into the row
+  *  @param row     row number of desired row
+  *  @param start   what column number to start at
+  */
+  public void setRowValues( float[] values, int row, int start );
+  
+ /**
+  * Get values for a portion or all of a column.
+  * The "from" and "to" values must be direct array reference, i.e.
+  * because the array positions start at zero, not one, this must be
+  * accounted for. If the array passed in exceeds the bounds of the array, 
+  * get values for array elements and ignore extra values.
+  *
+  *  @param  column  column number of desired column
+  *  @param  from    the row number of first element to be altered
+  *  @param  to      the row number of the last element to be altered
+  *  @return If column, from, and to are valid, an array of floats containing
+  *	     the specified section of the row is returned.
+  *	     If row, from, or to are invalid, an empty 1-D array is returned.
+  */
+  public float[] getColumnValues( int column, int from, int to );
+  
+ /**
+  * Set values for a portion or all of a column.
+  * The "from" and "to" values must be direct array reference, i.e.
+  * because the array positions start at zero, not one, this must be
+  * accounted for. If the array passed in exceeds the bounds of the array, 
+  * set values for array elements and ignore extra values.
+  *
+  *  @param values  array of elements to be put into the column
+  *  @param column  column number of desired column
+  *  @param start   what row number to start at
+  */
+  public void setColumnValues( float[] values, int column, int start );
+  
+ /**
+  * Get value for a single array element.
+  *
+  *  @param  row     row number of element
+  *  @param  column  column number of element
+  *  @return If element is found, the float value for that element is returned.
+  *	     If element is not found, zero is returned.
+  */ 
+  public float getDataValue( int row, int column );
+  
+ /**
+  * Set value for a single array element.
+  *
+  *  @param  row     row number of element
+  *  @param  column  column number of element
+  *  @param  value   value that element will be set to
+  */
+  public void setDataValue( int row, int column, float value );
+  
+ /**
+  * Returns the values in the specified region.
+  * The vertical dimensions of the region are specified by starting 
+  * at first row and ending at the last row. The horizontal dimensions 
+  * are determined by the first column and last column. 
+  *
+  *  @param  row_start  first row of the region
+  *  @param  row_stop	last row of the region
+  *  @param  col_start  first column of the region
+  *  @param  col_stop	last column of the region
+  *  @return If a portion of the array is specified, a 2-D array copy of 
+  *	     this portion will be returned. 
+  *	     If all of the array is specified, a reference to the actual array
+  *	     will be returned.
+  */
+  public float[][] getRegionValues( int row_start, int row_stop,
+				    int col_start, int col_stop );
+ /**  
+  * Sets values for a specified rectangular region. This method takes 
+  * in a 2D array that is already organized into rows and columns
+  * corresponding to a portion of the virtual array that will be altered.
+  *
+  *  @param  values	2-D array of float values 
+  *  @param  row_start  first row of the region being altered
+  *  @param  col_start  first column of the region being altered
+  */
+  public void setRegionValues( float[][] values, 
+			       int row_start,
+        		       int col_start );
+        		       
+ /**
+  * Returns number of rows in the array.
+  *
+  *  @return This returns the number of rows in the array. 
+  */ 
+  public int getNumRows();
 
-  /**
-   * Returns number of columns in the array.
-   */   
-   public int getNumColumns();
-      
+ /**
+  * Returns number of columns in the array.
+  *
+  *  @return This returns the number of columns in the array. 
+  */
+  public int getNumColumns();
+     
 }
