b3048af4f3d11e84de0f2233b77695cae883f7bc
millermi <millermi@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2003-12-12 06:11:44 +0000

    - Completely renovated how the points are selected.   Previously slope was used to restrict points, now   the angle of the point is calculated and compared   with the starting/ending angles of the arc. - This class now assumes the arc will be drawn   counterclockwise, so the starting angle may be   larger than the stopping angle if the 4th to   1st quadrant selection is made.

--- anl/ipns/ViewTools/Components/Region/WedgeRegion.java
+++ anl/ipns/ViewTools/Components/Region/WedgeRegion.java
@@ -34,6 +34,16 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.4  2003/12/12 06:11:44  millermi
+ *  - Completely renovated how the points are selected.
+ *    Previously slope was used to restrict points, now
+ *    the angle of the point is calculated and compared
+ *    with the starting/ending angles of the arc.
+ *  - This class now assumes the arc will be drawn
+ *    counterclockwise, so the starting angle may be
+ *    larger than the stopping angle if the 4th to
+ *    1st quadrant selection is made.
+ *
  *  Revision 1.3  2003/10/22 20:26:09  millermi
  *  - Fixed java doc errors.
  *
@@ -42,7 +52,7 @@
  *    that was commented out.
  *
  *  Revision 1.1  2003/08/21 18:21:36  millermi
- *  - Initial Version - uses equation for elipse and slope of a line to
+ *  - Initial Version - uses equation for ellipse and slope of a line to
  *    determine if a point is in the region.
  *
  */ 
@@ -51,6 +61,7 @@ package DataSetTools.components.View.Region;
 import java.awt.Point;
 import java.util.Vector;
  
+import DataSetTools.util.floatPoint2D;
 import DataSetTools.components.View.Cursor.SelectionJPanel;
 
 /**
@@ -70,8 +81,8 @@ import DataSetTools.components.View.Cursor.SelectionJPanel;
  * p[5].y = degrees covered by arc.
  *
  * The large number of defining points replaces the work of recalculating
- * information at each step of the region calculation. Like a ElipseRegion,
- * this region may appear to be circular, but may actually be eliptical.
+ * information at each step of the region calculation. Like a EllipseRegion,
+ * this region may appear to be circular, but may actually be elliptical.
  */ 
 public class WedgeRegion extends Region
 {
@@ -91,7 +102,7 @@ public class WedgeRegion extends Region
    
   /**
    * Get all of the points inside the wedge region. This method first determines
-   * if the point is in the elipse, then uses slope to find if the point is
+   * if the point is in the ellipse, then uses angles to find if the points
    * in the wedge region. 
    *
    *  @return array of points included within the wedge region.
@@ -108,88 +119,73 @@ public class WedgeRegion extends Region
      */
      Point center = new Point( definingpoints[0] );
      Point p1 = new Point( definingpoints[1] );
-     Point rp1 = new Point( definingpoints[2] );
+     Point rp1 = new Point( definingpoints[2] );   // reflection of p1
      Point topleft = new Point( definingpoints[3] );
      Point bottomright = new Point( definingpoints[4] );
      double xextent = (double)(bottomright.x - topleft.x)/2;
      double yextent = (double)(bottomright.y - topleft.y)/2;
-     // p1 will always be before rp1 when tracing unit circle counterclockwise
-     // if degrees covered by arc are negative, circle is missing a wedge. 
-     boolean counterclockwise = true;
-     if( definingpoints[5].y < 0 )
-       counterclockwise = false;
-     //square the extents for comparison later
-     xextent = Math.pow(xextent,2);
-     yextent = Math.pow(yextent,2);
-     
+     floatPoint2D fcenter = new floatPoint2D( (float)(topleft.x + xextent),
+                               (float)(topleft.y + yextent) );     
      Vector points = new Vector();
-     int pointangle = 0;
-     int stopangle = definingpoints[5].x + definingpoints[5].y;
-     if( stopangle < 0 )
-       stopangle = 360 + stopangle;
-     else if( stopangle > 360 )
-       stopangle = stopangle - 360;
      
-     // upper and lower slope is determined by p1 & rp1, midslope is per point
-     float upperslope = 0;
-     float lowerslope = 0;
-     float midslope   = 0;
-     
-     int p1quad = 0;  // the quadrant p1 is in, normal unit circle quadrants.
-     if( p1.x > center.x )
+     int startangle = definingpoints[5].x;
+     int totalangle = definingpoints[5].y + startangle;
+     int stopangle = totalangle;
+     int p1quad = 0;  
+     // using the startangle, find the quadrant of p1
+     if( startangle <= 180 )
      {
-       if( p1.y <= center.y )
-         p1quad = 1;
+       if( startangle <= 90 )
+       {
+         p1quad =  1;
+       }
        else
-         p1quad = 4;
+       {
+         p1quad =  2;
+       }
      }
      else
      {
-       if( p1.y < center.y )
-         p1quad = 2;
+       if( startangle < 270 )
+       {
+         p1quad =  3;
+       }
        else
-         p1quad = 3;
-     }
-     
-     int rp1quad = 0; // quadrant rp1 is in
-     if( rp1.x > center.x )
+       {
+         p1quad =  4;
+       }
+     } 
+     // make sure angle is between 0 and 360  
+     if( totalangle >= 360 )
+       totalangle -= 360;
+     //System.out.println("Total: " + totalangle );
+     int rp1quad = 0;
+     // using the startangle + arcangle, find quadrant of reflection of p1
+     if( totalangle <= 180 )
      {
-       if( rp1.y <= center.y )
-         rp1quad = 1;
+       if( totalangle <= 90 )
+       {
+         rp1quad =  1;
+       }
        else
-         rp1quad = 4;
+       {
+         rp1quad =  2;
+       }
      }
      else
      {
-       if( rp1.y < center.y )
-         rp1quad = 2;
+       if( totalangle < 270 )
+       {
+         rp1quad =  3;
+       }
        else
-         rp1quad = 3;
+       {
+         rp1quad =  4;
+       }
      }
-     
      //System.out.println("p1/rp1 Quad: " + p1quad + "/" + rp1quad );
      
-     // this code will avoid slope of 0 or +/- infinity
-     float numerator = p1.y - center.y;
-     if( numerator == 0 )
-       numerator = .01f;
-     float denominator = p1.x - center.x;
-     if( denominator == 0 )
-       denominator = -.01f;
-     lowerslope = numerator/denominator;
-     
-     numerator = rp1.y - center.y;
-     if( numerator == 0 )
-       numerator = .01f;
-     denominator = rp1.x - center.x;
-     if( denominator == 0 )
-       denominator = -.01f;
-     upperslope = numerator/denominator;
-     
-     //lowerslope = (float)(p1.y - center.y)/(float)(p1.x - center.x);
-     //upperslope = (float)(rp1.y - center.y)/(float)(rp1.x - center.x);
-    //System.out.println("Lower/Upper Slope: " + lowerslope + "/" + upperslope);
-     // using formula for elipse: (x-h)^2/a^2 + (y-k)^2/b^2 = 1
+     // using formula for ellipse: (x-h)^2/a^2 + (y-k)^2/b^2 = 1
      // where x,y is point, (h,k) is center, and a,b are x/y extent (radius)
      int quadnum = 0;
      // these values are specific to each quadrant, once quadrant is know, set
@@ -198,193 +194,109 @@ public class WedgeRegion extends Region
      int ystop = 0;
      int xstart = 0;
      int xstop = 0;
-     if( counterclockwise )
+     
+     // if rp1quad < p1quad, angle goes from 4th quad to 1st quad. so
+     // adjust rp1quad so it works as a ending bound for the for loop.
+     if( rp1quad < p1quad )
+       rp1quad += 4;
+     // if the two are in the same quad, but the total angle is greater than 90,
+     // the shape must be a pie with a slice removed.
+     else if( rp1quad == p1quad && definingpoints[5].y > 90 )
+       rp1quad += 3;
+     //System.out.println("p1quad/rp1quad: " + p1quad + "/" + rp1quad );
+     // Step through each quadrant involved in the selection.
+     for( int quadcount = p1quad; quadcount <= rp1quad; quadcount++ )
      {
-       if( rp1quad < p1quad )
-         rp1quad += 4;
-       for( int quadcount = p1quad; quadcount <= rp1quad; quadcount++ )
-       {
-         if(quadcount > 4 )
-           quadnum = quadcount - 4;
-         else
-	   quadnum = quadcount;
-	 
-	 if( quadnum == 1 )
-	 {
-	   ystart = topleft.y;
-	   ystop  = center.y;
-	   xstart = center.x + 1;
-	   xstop  = bottomright.x;
-	 }
-	 else if( quadnum == 2 )
-	 {
-	   ystart = topleft.y;
-	   ystop  = center.y - 1;
-	   xstart = topleft.x;
-	   xstop  = center.x;
-	 }
-	 else if( quadnum == 3 )
-	 {
-	   ystart = center.y;
-	   ystop  = bottomright.y ;
-	   xstart = topleft.x;
-	   xstop  = center.x;
-	 }
-	 else if( quadnum == 4 )
-	 {
-	   ystart = center.y + 1;
-	   ystop  = bottomright.y;
-	   xstart = center.x + 1;
-	   xstop  = bottomright.x;
-	 }
-	   
-         //System.out.println("Current quad(c): " + quadnum );
-
-     	 for( int y = ystart; y <= ystop; y++ )
-         {
-     	   for( int x = xstart; x <= xstop; x++ )
-     	   {
-     	     double dist = Math.pow((x - center.x),2)/xextent + 
-                	   Math.pow((y - center.y),2)/yextent;
-             //System.out.println("(" + x + "," + y + ")..." + dist ); 
-             if( dist <= 1 )
-             {
-	       if( p1quad != quadnum && rp1quad != quadnum && 
-	           (rp1quad - 4) != quadnum )
-	       {
-	         points.add( new Point( x, y ) );
-	       }
-	       else
-	       {
-	       //midslope = (float)(y - center.y)/(float)( x - center.x );
-	         
-                 numerator = y - center.y;
-                 if( numerator == 0 )
-                   numerator = .01f;
-                 denominator = x - center.x;
-                 if( denominator == 0 )
-                   denominator = -.01f;
-     	         midslope = numerator/denominator;
-	         
-                 if( rp1quad != quadnum )
-                 {
-                   if( midslope <= lowerslope )
-     	             points.add( new Point( x, y ) );
-                 }
-                 else if( p1quad != quadnum )
-                 {
-                   if( midslope >= upperslope )
-     	             points.add( new Point( x, y ) );
-                 }
-                 else
-                 {
-                   if( midslope <= lowerslope &&
-     	               midslope >= upperslope )
-     	             points.add( new Point( x, y ) );
-                 }
-               }
-             }
-           } // end for x
-         } // end for y
-       } // for quad
-     } // if counterclockwise
-     // *********************else its clockwise**********************
-     else
-     {
-       if( rp1quad >= p1quad )
-         p1quad += 4;
-       int quadenteredcount = 0;
-       for( int quadcount = p1quad; quadcount >= rp1quad; quadcount-- )
-       {
-         if(quadcount > 4 )
-           quadnum = quadcount - 4;
-	 else
-	   quadnum = quadcount;
-	 
-	 // set starting and ending points of the for loop depending on the quad
-	 if( quadnum == 1 )
-	 {
-	   ystart = topleft.y;
-	   ystop  = center.y;
-	   xstart = center.x + 1;
-	   xstop  = bottomright.x;
-	 }
-	 else if( quadnum == 2 )
-	 {
-	   ystart = topleft.y;
-	   ystop  = center.y - 1;
-	   xstart = topleft.x;
-	   xstop  = center.x;
-	 }
-	 else if( quadnum == 3 )
-	 {
-	   ystart = center.y;
-	   ystop  = bottomright.y ;
-	   xstart = topleft.x;
-	   xstop  = center.x;
-	 }
-	 else if( quadnum == 4 )
-	 {
-	   ystart = center.y + 1;
-	   ystop  = bottomright.y;
-	   xstart = center.x + 1;
-	   xstop  = bottomright.x;
-	 }
+       // since rp1quad could be > 4, restrict the quadnum to max of 4.
+       if(quadcount > 4 )
+     	 quadnum = quadcount - 4;
+       else
+         quadnum = quadcount;
        
-         //System.out.println("Current quad: " + quadnum );
-	 
-         if( quadenteredcount < 4 )
-         { 
-	   quadenteredcount++;
-     	   for( int y = ystart; y <= ystop; y++ )
-           {
-     	     for( int x = xstart; x <= xstop; x++ )
-     	     {
-     	       double dist = Math.pow((x - center.x),2)/xextent + 
-        		     Math.pow((y - center.y),2)/yextent;
-               //System.out.println("(" + x + "," + y + ")..." + dist ); 
-               if( dist <= 1 )
-               {
-	         if( p1quad != quadnum && (p1quad - 4) != quadnum && 
-		     rp1quad != quadnum )
-		 {
-		   points.add( new Point( x, y ) );
-		 }
-		 else
-		 {
-		   //midslope = (float)(y - center.y)/(float)( x - center.x );
-		   
-                   numerator = y - center.y;
-                   if( numerator == 0 )
-                     numerator = .01f;
-                   denominator = x - center.x;
-                   if( denominator == 0 )
-                     denominator = -.01f;
-     	           midslope = numerator/denominator;
-		   
-                   if( rp1quad != quadnum )
-                   {
-                     if( midslope >= lowerslope )
-     	               points.add( new Point( x, y ) );
-                   }
-                   else if( p1quad != quadnum && (p1quad - 4) != quadnum )
-                   {
-                     if( midslope <= upperslope )
-     	               points.add( new Point( x, y ) );
-		   }
-                   else
-                   {
-                     if( midslope >= lowerslope ||
-     	                 midslope <= upperslope )
-     	               points.add( new Point( x, y ) );
-                   }
-                 }
-	       }
-             } // end for x
-           } // end for y
-         } // end if quadenteredcount
-       } // for quadcount
-     } // end else clockwise
+       // Depending on which quadrant, set the bounds for the two for loops
+       // below which will step through all the points in that quadrant
+       // and add ones contained in the selection.
+       // Since y values go top to bottom, the ystart and stop are switched
+       // and since quad 2 & 3 x values should go right to left, those xstart
+       // and stop are switched.
+       if( quadnum == 1 )
+       {
+         ystart = center.y;
+         ystop  = topleft.y;
+         xstart = center.x;
+         xstop  = bottomright.x;
+       }
+       else if( quadnum == 2 )
+       {
+         ystart = center.y;
+         ystop  = topleft.y;
+         xstart = topleft.x;
+         xstop  = center.x - 1;
+       }
+       else if( quadnum == 3 )
+       {
+         ystart = bottomright.y;
+         ystop  = center.y + 1;
+         xstart = topleft.x;
+         xstop  = center.x - 1;
+       }
+       else if( quadnum == 4 )
+       {
+         ystart = bottomright.y;
+         ystop  = center.y + 1;
+         xstart = center.x;
+         xstop  = bottomright.x;
+       }
+       //System.out.println("Current quad(c): " + quadnum );
+       double dist = 0;
+       double xdiff = 0;
+       double ydiff = 0;
+       // These two loops will check every point within the given quadrant
+       // and test to see if the point is in the ellipse from which the
+       // arc comes from.
+       for( int y = ystart; y >= ystop; y-- )
+       {
+         for( int x = xstart; x <= xstop; x++ )
+         {
+     	   xdiff = 0;
+           ydiff = 0;
+           // x/y diff represent x-h/y-k respectively
+     	   xdiff = Math.abs( (double)x - fcenter.x );
+     	   ydiff = Math.abs( (double)y - fcenter.y );
+           // Subtracting 1/(xextent*4) is to account for fractional pixels.
+           // This will give a smoother, more accurate selected region.
+     	   dist = Math.pow((xdiff - 1/(xextent*4)),2)/Math.pow(xextent,2) + 
+        	  Math.pow((ydiff - 1/(yextent*4)),2)/Math.pow(yextent,2);
+     	   //System.out.println("(" + x + "," + y + ")..." + dist ); 
+     	   // Using ellipse equation, the distance must be < 1 in order to
+	   // be contained within the ellipse.
+	   if( dist <= 1 )
+     	   {
+	     int pointangle  = -Math.round( (float)Math.toDegrees(Math.atan2( 
+                                                    (double)(y - fcenter.y),
+                                                    (double)(x - fcenter.x))));
+             // put everything from 0-360
+             if( pointangle < 0 )
+               pointangle = 360 + pointangle;
+             
+     	    //System.out.println("Point/Stop: " + pointangle + "/" + stopangle);
+	     // if stopangle >= 360, the angle goes from 4th to 1st quadrant,
+	     // thus start - 360, and 0 - stop becomes the interval.
+	     if( stopangle >= 360 )
+	     {
+	       if( pointangle >= startangle || pointangle <= stopangle - 360 )
+        	 points.add( new Point( x, y ) );
+	     }
+	     // otherwise the angle must be between the start and stop angle.
+	     else
+	     {
+	       if( pointangle >= startangle && pointangle <= stopangle )
+        	 points.add( new Point( x, y ) );
+	     }
+     	   } // if( dist < 1 )
+     	 } // end for x
+       } // end for y
+     } // for quad
      
      // put the vector of points into an array of points
      selectedpoints = new Point[points.size()];
