0a167537c9ffaf4a062e6285ef9a7d344b371f52
millermi <millermi@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2003-12-29 20:59:29 +0000

    - Reduced imported packages.

--- anl/ipns/ViewTools/Components/Cursor/CircleCursor.java
+++ anl/ipns/ViewTools/Components/Cursor/CircleCursor.java
@@ -36,75 +36,80 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.2  2003/12/29 20:59:29  millermi
+ *  - Reduced imported packages.
+ *
  *  Revision 1.1  2003/05/24 17:32:20  dennis
  *  Initial version of cursor selection. (Mike Miller)
  *
  */
-
  package DataSetTools.components.View.Cursor;
 
- import javax.swing.*;
- import java.io.*;
- import java.awt.*;
+ import javax.swing.JPanel;
+ import java.io.Serializable;
+ import java.awt.Graphics;
+ import java.awt.Point;
  
- import DataSetTools.components.image.*;
+ import DataSetTools.components.image.XOR_Cursor;
 
 /** 
  *  This class implements a Rubberband Circle cursor for selecting regions.
+ *  Important to note is that this cursor is used to make EllipseRegions,
+ *  however, the cursor itself will always be circular. EllipseRegions were
+ *  used to generalize because scaling does not always yield a circular region.
  *
- *  @see  XOR_Cursor
+ *  @see  DataSetTools.components.image.XOR_Cursor
+ *  @see  DataSetTools.components.View.Region.EllipseRegion
  */
-
 public class CircleCursor extends  XOR_Cursor 
-                                implements Serializable
+                          implements Serializable
 {
-  /**
-   *  Construct a new CircleCursor to be used on a JPanel.
-   *
-   *  @param panel The JPanel for this cursor.
-   *
-   */
-   public CircleCursor ( JPanel panel ) 
-   {
-      super( panel );
-   }
+ /**
+  *  Construct a new CircleCursor to be used on a JPanel.
+  *
+  *  @param panel The JPanel for this cursor.
+  *
+  */
+  public CircleCursor ( JPanel panel ) 
+  {
+    super( panel );
+  }
 
-  /**
-   *  This method draws a circle with two specified points, the center and
-   *  a point on the radius.
-   *
-   *  @param  graphics   The graphics context that the circle will be drawn in.
-   *
-   *  @param  p1         Center point of the circle.
-   *
-   *  @param  p2         Radius point of the circle.
-   */
-   public void draw( Graphics graphics, Point p1, Point p2 )
-   {
-      int x = Math.abs( p1.x - p2.x );
-      int y = Math.abs( p1.y - p2.y );
-      int r = (int)Math.sqrt( Math.pow(x,2) + Math.pow(y,2) ); 
-    	           
-      graphics.drawOval( (p1.x - r), (p1.y - r), 2*r, 2*r );
-      graphics.drawLine( p1.x - 2, p1.y, p1.x + 2, p1.y );
-      graphics.drawLine( p1.x, p1.y - 2, p1.x, p1.y + 2 );
-   }
+ /**
+  *  This method draws a circle with two specified points, the center and
+  *  a point on the radius.
+  *
+  *  @param  graphics	The graphics context that the circle will be drawn in.
+  *
+  *  @param  p1 	Center point of the circle.
+  *
+  *  @param  p2 	Radius point of the circle.
+  */
+  public void draw( Graphics graphics, Point p1, Point p2 )
+  {
+    int x = Math.abs( p1.x - p2.x );
+    int y = Math.abs( p1.y - p2.y );
+    int r = (int)Math.sqrt( Math.pow(x,2) + Math.pow(y,2) ); 
+        	 
+    graphics.drawOval( (p1.x - r), (p1.y - r), 2*r, 2*r );
+    graphics.drawLine( p1.x - 2, p1.y, p1.x + 2, p1.y );
+    graphics.drawLine( p1.x, p1.y - 2, p1.x, p1.y + 2 );
+  }
 
 
-  /**
-   *  This method returns the region determined by the starting and
-   *  ending point of the circle cursor.
-   *
-   *  @return  A circle determined by the starting and ending point of 
-   *           the circle cursor
-   */
+ /**
+  *  This method returns the region determined by the starting and
+  *  ending point of the circle cursor.
+  *
+  *  @return  A circle determined by the starting and ending point of 
+  *	      the circle cursor
+  */
+  public Circle region() 
+  {
+    int x = Math.abs( first_pt.x - last_pt.x );
+    int y = Math.abs( first_pt.y - last_pt.y );
+    float r = (float)Math.sqrt( Math.pow(x,2) + Math.pow(y,2) );
 
-   public Circle region() 
-   {
-      int x = Math.abs( first_pt.x - last_pt.x );
-      int y = Math.abs( first_pt.y - last_pt.y );
-      float r = (float)Math.sqrt( Math.pow(x,2) + Math.pow(y,2) );
-
-      return new Circle( first_pt, r );
-   }
+    return new Circle( first_pt, r );
+  }
 }
