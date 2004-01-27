582e617c714a35a19db6ae3388d719c567fe3d02
Dennis Mikkelson <mikkelsond@uwstout.edu>
2004-01-27 20:37:58 +0000

    Added methods to get/set all relevant paramters, such as the plane, width, height, depth and step size. Added a StepListener, which calculates and sets a new plane when the < or > buttons are pressed.

--- anl/ipns/ViewTools/UI/SliceSelectorUI.java
+++ anl/ipns/ViewTools/UI/SliceSelectorUI.java
@@ -30,6 +30,12 @@
  * Modified:
  *
  * $Log$
+ * Revision 1.2  2004/01/27 20:37:58  dennis
+ * Added methods to get/set all relevant paramters, such as the
+ * plane, width, height, depth and step size.
+ * Added a StepListener, which calculates and sets a new plane
+ * when the < or > buttons are pressed.
+ *
  * Revision 1.1  2004/01/26 23:55:12  dennis
  * Initial version of user interface for selecting
  * a plane, image size and stepping a
@@ -40,9 +46,9 @@
 package DataSetTools.components.ui;
 
 import DataSetTools.util.*;
+import DataSetTools.math.*;
 import java.awt.*;
 import java.awt.event.*;
-//import java.util.*;
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.border.*;
@@ -54,10 +60,9 @@ import java.io.*;
  */
 
 public class SliceSelectorUI extends    ActiveJPanel
-                             implements Serializable 
+                             implements ISlicePlaneSelector,
+                                        Serializable 
 {
-  public static final String VALUE_CHANGED = "Value Changed";
-  
   private SlicePlane3D_UI plane_selector;
   private SliceImageUI    image_selector;
   private SliceStepperUI  stepper;
@@ -89,7 +94,131 @@ public class SliceSelectorUI extends    ActiveJPanel
     ValueListener value_listener = new ValueListener();
     plane_selector.addActionListener( value_listener );
     image_selector.addActionListener( value_listener );
-    stepper.addActionListener( value_listener );
+
+    stepper.addActionListener( new StepListener() );
+  }
+
+
+  /* ---------------------------- setPlane ----------------------------- */
+  /**
+   *  Set the plane whose parameters are to be displayed.
+   *
+   *  @param new_plane  The plane to display.
+   */
+  public void setPlane( SlicePlane3D new_plane )
+  {
+    plane_selector.setPlane( new_plane );
+  }
+
+
+  /* ---------------------------- getPlane ----------------------------- */
+  /**
+   *  Get the currently specified plane, which may be null if the user
+   *  supplied values don't specifiy a valid plane.
+   *
+   *  @return the currently specified plane, or null if there is no valid
+   *          plane specified.
+   */
+  public SlicePlane3D getPlane()
+  {
+    return plane_selector.getPlane();
+  }
+
+
+  /* ------------------------- setStepSize --------------------------- */
+  /**
+   *  Set the current step size, MUST be positive.
+   *
+   *  @param new_step  The new step size to use, MUST be positive.
+   */
+  public void setStepSize( float new_step )
+  {
+    image_selector.setStepSize( new_step );
+  }
+
+
+  /* ---------------------------- getStepSize ---------------------------- */
+  /**
+   *  Get the currently selected step size.
+   *
+   *  @return the currently selected steps/unit.
+   */
+  public float getStepSize()
+  {
+    return image_selector.getStepSize();
+  }
+
+
+  /* --------------------------- setSliceWidth --------------------------- */
+  /**
+   *  Set the current width, MUST be positive.
+   *
+   *  @param  new_width  The new width to use, MUST be positive.
+   */
+  public void setSliceWidth( float new_width )
+  {
+    image_selector.setSliceWidth( new_width );
+  }
+
+
+  /* --------------------------- getSliceWidth --------------------------- */
+  /**
+   *  Get the currently selected width.
+   *
+   *  @return the currently selected width.
+   */
+  public float getSliceWidth()
+  {
+    return image_selector.getSliceWidth();
+  }
+
+
+  /* -------------------------- setSliceHeight --------------------------- */
+  /**
+   *  Set the current height, MUST be positive.
+   *
+   *  @param  new_height  The new height to use, MUST be positive.
+   */
+  public void setSliceHeight( float new_height )
+  {
+     image_selector.setSliceHeight( new_height );
+  }
+
+
+  /* -------------------------- getSliceHeight --------------------------- */
+  /**
+   *  Get the currently selected height.
+   *
+   *  @return the currently selected height.
+   */
+  public float getSliceHeight()
+  {
+    return image_selector.getSliceHeight();
+  }
+
+
+  /* ------------------------- setSliceThickness ------------------------- */
+  /**
+   *  Set the current thickness, MUST be positive.
+   *
+   *  @param  new_thickness  The new thickness to use, MUST be positive.
+   */
+  public void setSliceThickness( float new_thickness )
+  {
+
+    image_selector.setSliceThickness( new_thickness );
+  }
+
+
+  /* ------------------------- getSliceThickness ------------------------- */
+  /**
+   *  Get the currently selected thickness.
+   *
+   *  @return the currently selected thickness.
+   */
+  public float getSliceThickness()
+  {
+    return image_selector.getSliceThickness();
   }
 
 
@@ -104,11 +233,13 @@ public class SliceSelectorUI extends    ActiveJPanel
               + stepper.toString() + "\n";
   }
 
+
   /* -----------------------------------------------------------------------
    *
    *  PRIVATE CLASSES
    *
    */
+
   /* ------------------------ ValueListener ------------------------------ */
   /*
    *  Listen for a new value.
@@ -118,9 +249,34 @@ public class SliceSelectorUI extends    ActiveJPanel
     public void actionPerformed( ActionEvent e )
     {
       System.out.println("Value changed");
-      send_message( VALUE_CHANGED );
+      send_message( ISlicePlaneSelector.PLANE_CHANGED );
     }
   }
+
+
+  /* ------------------------ StepListener ------------------------------ */
+  /*
+   *  Listen for a new value.
+   */
+  private class StepListener implements ActionListener
+  {
+    public void actionPerformed( ActionEvent e )
+    {
+                                      // move the plane based on specified step
+      SlicePlane3D plane = plane_selector.getPlane();
+      Vector3D origin = plane.getOrigin();
+      Vector3D normal = plane.getNormal();
+      float step = stepper.getStep(); 
+      normal.normalize();
+      normal.multiply( step );
+      origin.add( normal );
+      plane.setOrigin( origin );
+      plane_selector.setPlane( plane );
+
+      send_message( ISlicePlaneSelector.PLANE_CHANGED );
+    }
+  }
+
 
   /* ------------------------------ main --------------------------------- */
   /**
@@ -129,7 +285,7 @@ public class SliceSelectorUI extends    ActiveJPanel
   public static void main( String args[] )
   {
     JFrame  f = new JFrame("Test for SliceSelectorUI");
-    f.setBounds( 0, 0, 210, 300 ); 
+    f.setBounds( 0, 0, 210, 350 ); 
 
     final SliceSelectorUI test = new SliceSelectorUI("HKL Slice");
 
