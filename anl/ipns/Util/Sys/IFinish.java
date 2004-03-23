17cfda7cf9ce2a4806d979a96d68ec06902f460b
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2004-03-23 14:44:24 +0000

    Initial checkin

---
+++ anl/ipns/Util/Sys/IFinish.java
@@ -0,0 +1,59 @@
+
+/*
+ * File:  IFinish.java 
+ *             
+ * Copyright (C) 2004, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Ruth Mikkelson <mikkelsonr@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ * This work was supported by the National Science Foundation under
+ * grant number DMR-0218882
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ *
+ * Modified:
+ *
+ * $Log$
+ * Revision 1.1  2004/03/23 14:44:24  rmikk
+ * Initial checkin
+ *
+ */
+
+package gov.anl.ipns.Util.Sys;
+
+
+
+/**
+  *  Classes that implement this interface contain a finish method.  If the 
+  *  class is an instance of a Window, it should dispose of the window and 
+  *  ALSO finalize the window
+  */
+
+public interface IFinish{
+
+  /**
+    *  If the class that implements this interface is a Window, it should 
+    *  dispose and finalize the window
+    */ 
+  public void finish();
+}
