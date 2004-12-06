fa8faae4aa18f559671d22302cb622b5c25ce51d
ffr <ffr@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2004-12-06 00:31:55 +0000

    Removed addPlot with title. Added some more comments.

--- anl/ipns/ViewTools/Displays/IOneDPlot.java
+++ anl/ipns/ViewTools/Displays/IOneDPlot.java
@@ -1,30 +1,15 @@
 /*
  * Created on 9/06/2004
  * 
- * @author Ferdi Franceschini
- * The Bragg Institute
- * Australian Nuclear Science and Technology Organisation
- * ffr@ansto.gov.au
+ * @author Ferdi Franceschini The Bragg Institute Australian Nuclear Science and
+ * Technology Organisation ffr@ansto.gov.au
  */
 package gov.anl.ipns.ViewTools.Displays;
 
 import java.awt.Color;
+import java.awt.Rectangle;
 
 public interface IOneDPlot {
-    /**
-     * Add a plot to an existing graph and set the title
-     * 
-     * @param x
-     *            array of x data
-     * @param y
-     *            array of y data
-     * @param yerr
-     *            array of y errors, use null for no errors
-     * @param t
-     *            plot title
-     */
-    public void addPlot(float[] x, float[] y, float[] yerr, String title,
-            int gnum);
 
     /**
      * Add a plot to an existing graph
@@ -68,56 +53,49 @@ public interface IOneDPlot {
     /**
      * Set log scale on Y axis and redraw plot.
      * 
-     * @param scale true=log, false = linear
+     * @param scale
+     *            true=log, false = linear
      */
     public void setLogScaleY(boolean scale);
-    
+
     /**
      * Set log scale on X axis and redraw plot.
      * 
-     * @param scale true=log, false = linear
+     * @param scale
+     *            true=log, false = linear
      */
     public void setLogScaleX(boolean scale);
 
     /**
-     * Allows user to set the minimum with a single click on the plot.
-     * The maximum is set to the data maximum.
-     * TODO Maximum should auto-scale 
-     * @param x minimum
+     * Get pixel coordinates of top left corner of plot region
+     * with width and height.
+     * @return Rectangle
      */
-    public void setXScaleMin(float x);
-
-    public float getXScaleMin();
+    public Rectangle getPlotBounds();
 
     /**
-     * Allows user to set the maximum with a single click on the plot.
-     * The maximum is set to the data minimum.
-     * TODO Minimum should auto-scale 
-     * @param x maximum
+     * @return minimum X value from all plotted data sets
+     * or the minimum set by setXDataRange
      */
-    public void setXScaleMax(float x);
-
-    public float getXScaleMax();
+    public float getXPlotMin();
 
     /**
-     * Allows user to set the minimum with a single click on the plot.
-     * The maximum is set to the data maximum.
-     * TODO Maximum should auto-scale 
-     * @param y minimum
+     * @return maximum X value from all plotted data sets
+     * or the maximum set by setXDataRange
      */
-    public void setYScaleMin(float y);
-
-    public float getYScaleMin();
+    public float getXPlotMax();
 
     /**
-     * Allows user to set the maximum with a single click on the plot.
-     * The maximum is set to the data minimum.
-     * TODO Minimum should auto-scale 
-     * @param y maximum
+     * @return minimum Y value from all plotted data sets
+     * or the minimum set by setYDataRange
      */
-    public void setYScaleMax(float y);
+    public float getYPlotMin();
 
-    public float getYScaleMax();
+    /**
+     * @return maximum Y value from all plotted data sets
+     * or the maximum set by setYDataRange
+     */
+    public float getYPlotMax();
 
     /**
      * Plot a point without error bars
@@ -146,9 +124,13 @@ public interface IOneDPlot {
      */
     public int setPoint(float x, float y, float yerr, int gnum);
 
+    /**
+     * Use this to set the number of points to show in a live data plot
+     * @param np
+     */
     public void setNumPoints(int np);
 
-    public void setPlotTitle(String t);
+    public void setGraphTitle(String t);
 
     public void setYLabel(String t);
 
@@ -158,11 +140,57 @@ public interface IOneDPlot {
 
     public void setYUnits(String u);
 
-    public void setYScale(float min, float max);
+    /** Set Y data range for plot
+     * Note: Implementors are free to set the Y axis scale
+     * to leave space around the plotted data. 
+     * @param min
+     * @param max
+     */
+    public void setYDataRange(float min, float max);
 
-    public void setXScale(float min, float max);
+    /** Set X data range for plot
+     * Note: Implementors are free to set the X axis scale
+     * to leave space around the plotted data. 
+     * @param min
+     * @param max
+     */
+    public void setXDataRange(float min, float max);
 
+    /**
+     * Transform x screen coordinate to data coordinate
+     * 
+     * @param pixel count from x bound of the axes container
+     * @return
+     *            screen coordinate
+     */
+    public float xScreen2Data(float x);
+
+    /**
+     * Transform y screen coordinate to data coordinate
+     * 
+     * @param pixel count from y bound of the axes container
+     * @return
+     *            screen coordinate
+     */
+    public float yScreen2Data(float y);
+    
+    /**
+     * Autoscale Y axis
+     */
     public void autoYScale();
 
+    /**
+     * Autoscale X axis
+     */
     public void autoXScale();
+
+    /**
+     * @return true if X axis is auto-scaling
+     */
+    public boolean is_autoXScale();
+
+    /**
+     * @return true if Y axis is auto-scaling
+     */
+    public boolean is_autoYScale();
 }
