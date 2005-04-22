08cefbf079728eb2eeb6b0683898e1926ec4d0bb
kramer <kramer@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2005-04-22 20:33:56 +0000

    Object that describes the contours on a contour graph where the contours are evenly spaced.

---
+++ anl/ipns/ViewTools/Panels/Contour/Contours/UniformContours.java
@@ -0,0 +1,35 @@
+/*
+ * Created on Apr 19, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package gov.anl.ipns.ViewTools.Panels.Contour.Contours;
+
+public class UniformContours extends Contours
+{
+   private float minValue;
+   private float delta;
+   
+   public UniformContours(float minValue,
+                        float maxValue,
+                        int numLevels) throws IllegalArgumentException
+   {
+      super(numLevels);
+      
+      if (minValue>maxValue)
+         throw new IllegalArgumentException(
+                   "ContourJPanel$UniformLevels(float minValue, " +
+                   "float maxValue, int numLevels) was improperly " +
+                   "given 'minValue' and 'maxValue' such that " +
+                   "'minValue'>'maxValue'");
+      
+      this.minValue = minValue;
+      this.delta = (maxValue-minValue)/numLevels;
+   }
+
+   public float getLevelAt(int i)
+   {
+      return minValue+i*delta;
+   }
+}
