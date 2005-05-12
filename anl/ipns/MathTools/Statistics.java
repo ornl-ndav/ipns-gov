7f4211f26edaef7829cbc214386a2512e2653ea9
Dennis Mikkelson <mikkelsond@uwstout.edu>
2005-05-12 15:21:57 +0000

    Added methods for mean, min, max and standard deviation.

--- anl/ipns/MathTools/Statistics.java
+++ anl/ipns/MathTools/Statistics.java
@@ -30,6 +30,9 @@
  * Modified:
  * 
  *  $Log$
+ *  Revision 1.6  2005/05/12 15:21:57  dennis
+ *  Added methods for mean, min, max and standard deviation.
+ *
  *  Revision 1.5  2004/03/11 21:57:31  dennis
  *  Changed package to java gov.anl.ipns.MathTools.Statistics
  *
@@ -61,7 +64,6 @@ public final class Statistics
    *  @return   Returns an array with two entries, the slope and y-intercept
    *            for the best fit line. 
    */
-
   public static float[] FitLine( float x_vals[], float y_vals[] )
   {
     float parameters[] = new float[2];
@@ -99,10 +101,122 @@ public final class Statistics
     parameters[1] = (float)((sum_x2 * sum_y - sum_x * sum_xy) / determinant);
     return parameters;
   }
+
+
+  /**
+   *  Find the mean of all of the values stored in the specified array
+   *  
+   *  @param  arr   Float Array of values, there must be at least 1 value
+   *
+   *  @return   Returns a float with the mean of the array.
+   */
+  public static float mean(float[] arr)
+  {
+     if ( arr == null || arr.length == 0 )
+     {
+       System.out.println("WARNING: arr null or length 0 in Statistics.mean");
+       return Float.NaN;
+     }
+
+     float total = 0;   
+     for( int i = 0; i < arr.length; i++ ) 
+       total = total + arr[i];   
+     return (total / arr.length );
+  }
+
+
+  /**
+   *  Find the maximum of all of the values stored in the specified array
+   *  
+   *  @param  arr   Float Array of values, there must be at least 1 value
+   *
+   *  @return   Returns a float with the maximum value of the array.
+   */
+  public static float maximum( float[] arr )
+  {
+     if ( arr == null || arr.length == 0 )
+     {
+       System.out.println("WARNING: arr null or length 0 in " + 
+                          "Statistics.maximum");
+       return Float.NaN;
+     }
+
+     float highest = arr[0];
+     for( int i = 1; i < arr.length; i++ )
+       if ( highest < arr[i] )
+          highest = arr[i];
+
+     return highest;
+  }
+
+
+  /**
+   *  Find the minimum of all of the values stored in the specified array
+   *  
+   *  @param  arr   Float Array of values, there must be at least 1 value
+   *
+   *  @return   Returns a float with the maximum value of the array.
+   */
+  public static float minimum( float[] arr )
+  {
+     if ( arr == null || arr.length == 0 )
+     {
+       System.out.println("WARNING: arr null or length 0 in " + 
+                          "Statistics.minimum");
+       return Float.NaN;
+     }
+
+     float lowest = arr[0];
+     for( int i = 1; i < arr.length; i++ )
+       if ( lowest > arr[i] )
+          lowest = arr[i];
+
+     return lowest;
+  }
+
+
+  /**
+   *  Find the standard deviation of all of the values stored in the 
+   *  specified array
+   *  
+   *  @param  arr   Float Array of values, there must be at least 1 value
+   *
+   *  @return   Returns a float with the standard deviation of the 
+   *            values in array.
+   */
+  public static float std_deviation( float[] arr )
+  {
+     if ( arr == null || arr.length == 0 )
+     {
+       System.out.println("WARNING: arr null or length 0 in " +
+                          "Statistics.std_deviation");
+       return Float.NaN;
+     }
+
+     float sum    = 0;
+     float sum_sq = 0;
+     int   N      = arr.length;
+     for( int i = 0; i < N; i++ )
+     {
+       sum    += arr[i];
+       sum_sq += arr[i] * arr[i];
+     }
+     return (float)( Math.sqrt( N*sum_sq - sum*sum ) / N );
+  }
+
   
   public static void main( String args[] )
   {
-    System.out.println("Empty Main");
+    float xvals[] = {1,2,3};     
+    float yvals[] = {3,5,7};     
+
+    float params[] = FitLine( xvals, yvals );
+    System.out.println("Fit parameters = " + params[0] + ", " + params[1] );
+
+    System.out.println("mean    = " + mean( xvals ) );
+    System.out.println("minimum = " + minimum( xvals ) );
+    System.out.println("maximum = " + maximum( xvals ) );
+    System.out.println("std dev = " + std_deviation( xvals ) );
   }
 
 }
