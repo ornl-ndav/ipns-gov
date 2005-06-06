2e1ed296821b919d7d3d1a618d2871fd3c7f296b
kramer <kramer@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2005-06-06 14:34:53 +0000

    Now the paint() method finds the point on a contour with its gradient closest to 0 and draws the contour's value at this point (the value is rotated when it is drawn to be parallel to the contour line's tangent at that point).

--- anl/ipns/ViewTools/Panels/Contour/ContourJPanel.java
+++ anl/ipns/ViewTools/Panels/Contour/ContourJPanel.java
@@ -19,6 +19,8 @@ import gov.anl.ipns.ViewTools.Panels.Transforms.CoordTransform;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 import java.io.Serializable;
 import java.util.Vector;
 
@@ -119,11 +121,30 @@ public class ContourJPanel extends CoordJPanel implements Serializable,
       this.levels = new NonUniformContours(levels);
    }
    
+   public void changeData(IVirtualArray2D arr)
+   {
+      data2D = arr;
+      repaint();
+   }
+   
+   public Contours getContours()
+   {
+      return levels;
+   }
+   
+   public void setContours(Contours levels)
+   {
+      this.levels = levels;
+      repaint();
+   }
+   
    /**
     * Does the actual work of painting the contour lines on the panel.
     */
-   public void paint(Graphics g)
+   public void paint(Graphics gr)
    {
+      Graphics2D g = (Graphics2D)gr;
+      
       stop_box( current_point, false );   // if the system redraws this without
 
       stop_crosshair( current_point );    // our knowlege, we've got to get rid
@@ -166,13 +187,24 @@ public class ContourJPanel extends CoordJPanel implements Serializable,
       //this will hold the x points describing where each contour line segment 
       //that should be drawn.  The units will be in terms of row/column and 
       //the numbers are in the order p1_start, p1_end, p2_start, p2_end ....
-          float[] xrcVals;
+        float[] xrcVals;
       //the same as above except this stores y values
-          float[] yrcVals;
+        float[] yrcVals;
+      //this is the index of the point with the minimum gradient on a level
+      //(where grad(F(x,y)) := F_x(x,y)*i + F_y(x,y)*j where 
+      // F_x(x,y) denotes the partial derivative of F with respect to x
+      // F_y(x,y) is the partial derivative of F with respect to y)
+        int minGradIndex;
+      //this stores the square of the minimum gradient
+        float minGradSqr;
         
       //for each level draw the contour
       for (int i=0; i<levels.getNumLevels(); i++)
       {
+         //first to initialize the variables used to draw this level
+           minGradIndex = 0;
+           minGradSqr = Float.MAX_VALUE;
+         
          //now to get the contours
            contourPts = Contour2D.contour(arr,levels.getLevelAt(i));
          
@@ -184,8 +216,38 @@ public class ContourJPanel extends CoordJPanel implements Serializable,
            for (int j=0; j<contourPts.size(); j++)
            {
               curPt = (floatPoint2D)contourPts.elementAt(j);
-              xrcVals[j] = curPt.x;
-              yrcVals[j] = curPt.y;
+              xrcVals[j] = curPt.x+1/2f;
+              yrcVals[j] = curPt.y+1/2f;
+              /* Note:  1/2f is added to each point for the following reason.
+               *        First, curPt.x (and curPt.y) return a point related 
+               *        in terms of rows and columns.  However, the 
+               *        row/column description assumes that data points 
+               *        over a square on the grid of row/columns are "drawn" 
+               *        at the top-left corner of the square.  However, the 
+               *        transforms here assumes the points are "drawn" at the 
+               *        center of the boxes.  Adding 1/2f shifts the point 
+               *        from the corner to the center.
+               */
+              
+              //now to determine the square of the gradient of the
+              //function at this point
+              int xIndex = (int)curPt.x;
+              int yIndex = (int)curPt.y;
+              if ( xIndex>=0 && yIndex>=0 && yIndex<arr[xIndex].length && 
+                   xIndex+1<arr.length && yIndex<arr[xIndex+1].length)
+              {
+                 float Fx = arr[xIndex+1][yIndex] - arr[xIndex][yIndex];
+                 if ( yIndex+1<arr[xIndex].length )
+                 {
+                    float Fy = arr[xIndex][yIndex+1] - arr[xIndex][yIndex];
+                    float gradSqr = Fx*Fx+Fy*Fy;
+                    if (gradSqr<=minGradSqr)
+                    {
+                       minGradSqr = gradSqr;
+                       minGradIndex = j;
+                    }
+                 }
+              }
            }
          
          //now to transform the points from row/column to the world 
@@ -200,12 +262,75 @@ public class ContourJPanel extends CoordJPanel implements Serializable,
             local_transform.MapXListTo(xrcVals);
             local_transform.MapYListTo(yrcVals);
          
+         /* For Testing purposes
+         //first set the line color for this level
+         //for now this just for testing purposes
+           g.setColor(new Color((new Random()).nextInt()));
+         */
+            
          //now to draw the lines with the new pixel coordinates
            for (int j=0; (j+1)<contourPts.size(); j+=2)
-              g.drawLine((int)xrcVals[j],
-                         (int)(yrcVals[j]),
-                         (int)(xrcVals[j+1]),
-                         (int)(yrcVals[j+1]));
+           {
+              if (j==minGradIndex || (j+1)==minGradIndex)
+              {
+                 float avX = xrcVals[j];
+                 float avY = yrcVals[j];
+                 float angle = 0;
+                 if (j+1 < xrcVals.length && 
+                     j+1 < yrcVals.length)
+                 {
+                    avX = (float)(avX/2.0 + xrcVals[j+1]/2.0);
+                    avY = (float)(avY/2.0 + yrcVals[j+1]/2.0);
+                    
+                    //now to find the angle to use
+                    angle = (float)Math.atan((yrcVals[j+1]-avY)/
+                                             (xrcVals[j+1]-avX));
+                 }
+                 //render the label for this level of the contour
+                 AffineTransform trans = g.getTransform();
+                 //double xTrans = trans.getTranslateX();
+                 //double yTrans = trans.getTranslateY();
+                 //System.out.println("xTrans = "+xTrans+"\nyTrans = "+yTrans);
+                 //g.translate(-xTrans, -yTrans);
+                 g.translate(avX, avY);
+                 g.rotate(angle);
+                 g.translate(-avX, -avY);
+                 //g.translate(xTrans, yTrans);
+                 g.drawString(""+levels.getLevelAt(i),
+                               (int)(avX),
+                               (int)(avY));
+                 g.setTransform(trans);
+              }
+              //else
+              {
+                 g.drawLine((int)(xrcVals[j]),
+                       (int)(yrcVals[j]),
+                       (int)(xrcVals[j+1]),
+                       (int)(yrcVals[j+1]));
+              }
+           }
+           
+         //now to draw a box around the data
+           float delta = 1/2f;
+           float[] xCorners = new float[2];
+             xCorners[0] = delta;
+             xCorners[1] = data2D.getNumRows()-1+delta;
+           float[] yCorners = new float[2];
+             yCorners[0] = delta;
+             yCorners[1] = data2D.getNumColumns()-1+delta;
+           //again transform the points from row/column to world
+           //coordinates
+             rcToGlobal.MapXListTo(xCorners);
+             rcToGlobal.MapYListTo(yCorners);
+           //then map the world coordinates to the 'local_transform'
+           //(i.e. the region the user is viewing)
+             local_transform.MapXListTo(xCorners);
+             local_transform.MapYListTo(yCorners);
+           //draw the box
+             g.drawRect((int)xCorners[0],
+                        (int)yCorners[0],
+                        (int)(xCorners[1]-xCorners[0]),
+                        (int)(yCorners[1]-yCorners[0]));
       }
    }
    
@@ -230,15 +355,15 @@ public class ContourJPanel extends CoordJPanel implements Serializable,
     * Generates a float[][] for the data for the function 
     * f(x,y) = x+y
     */
-   public static float[][] getTestDataArr()
+   private static float[][] getTestDataArr()
    {
-      float xMin = -10;
+      float xMin = 0;
       float xMax = 10;
-      int numXSteps = 40;
+      int numXSteps = 20;
       
-      float yMin = -10;
+      float yMin = 0;
       float yMax = 10;
-      int numYSteps = 40;
+      int numYSteps = 20;
       
       
       float deltaX = (xMax-xMin)/numXSteps;
@@ -251,7 +376,7 @@ public class ContourJPanel extends CoordJPanel implements Serializable,
          {
             x = (xMin+deltaX*i);
             y = (yMin+deltaY*j);
-            dataArr[i][j] = x*x+y*y-5;
+            dataArr[i][j] = x*x*y*y;
          }
       
       return dataArr;
@@ -261,7 +386,7 @@ public class ContourJPanel extends CoordJPanel implements Serializable,
     * Generates a IVirtualArray2D for the data for the function 
     * f(x,y) = x+y
     */
-   public static IVirtualArray2D getTestData()
+   private static IVirtualArray2D getTestData()
    {
       return new VirtualArray2D(getTestDataArr());
    }
