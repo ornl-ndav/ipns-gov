58688414268c78e6f16edc6090e13c1e23dd3fd2
kramer <kramer@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2005-06-08 17:22:02 +0000

    Added the GNU header, added javadocs, and modified the constructor so that if the array given to it is null, it calls super(1) (so that the superclass doesn't complain, instead this class throws an Exception).

--- anl/ipns/ViewTools/Panels/Contour/Contours/NonUniformContours.java
+++ anl/ipns/ViewTools/Panels/Contour/Contours/NonUniformContours.java
@@ -1,18 +1,65 @@
 /*
- * Created on Apr 19, 2005
+ * File: NonUniformContours.java
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * Copyright (C) 2005, Dominic Kramer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Primary   Dominic Kramer <kramerd@uwstout.edu>
+ * Contact:  Student Developer, University of Wisconsin-Stout
+ *           
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0218882, and by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ * For further information, see <http://www.pns.anl.gov/ISAW/>
+ *
+ * Modified:
+ * $Log$
+ * Revision 1.3  2005/06/08 17:22:02  kramer
+ * Added the GNU header, added javadocs, and modified the constructor so
+ * that if the array given to it is null, it calls super(1) (so that the
+ * superclass doesn't complain, instead this class throws an Exception).
+ *
  */
 package gov.anl.ipns.ViewTools.Panels.Contour.Contours;
 
+/**
+ * Represents a set of contour levels of any size and arrangement.
+ */
 public class NonUniformContours extends Contours
 {
+   /** Holds the value of each contour level. */
    private float[] levels;
    
+   /**
+    * Create a set of any number and arrangement of contour levels.
+    * 
+    * @param levels The values of the contour levels.
+    * @throws IllegalArgumentException If <code>levels</code> is 
+    *                                  <code>null</code> or if 
+    *                                  <code>levels.length==0</code>.
+    */
    public NonUniformContours(float[] levels) throws IllegalArgumentException
    {
-      super((levels!=null)?levels.length:0);
+      super((levels!=null)?levels.length:1);
       if (levels==null)
          throw new IllegalArgumentException(
                      "ContourJPanel$NonUniformLevels(float[] levels) " +
@@ -20,6 +67,16 @@ public class NonUniformContours extends Contours
       this.levels = levels;
    }
 
+   /**
+    * Get the value of the <code>ith</code> contour level.
+    * 
+    * @param The index of the contour level to reference.  For <code>i</code> 
+    *        to be valid, it must be in the range 
+    *        [0,{@link Contours#getNumLevels() Contours.getNumLevels()}).
+    * @return The value of the <code>ith</code> contour level or 
+    *         {@link Float#NaN Float.NaN} if <code>i</code> is invalid.
+    * @see Contours#getLevelAt(int)
+    */
    public float getLevelAt(int i)
    {
       if (i>=0 && i<levels.length)
@@ -28,6 +85,12 @@ public class NonUniformContours extends Contours
          return Float.NaN;
    }
    
+   /**
+    * Used to get a reference to all of the contour levels.  Note:  The array 
+    * returned is not sorted in any way.
+    * 
+    * @return All of the contour levels in this set.
+    */
    public float[] getLevels()
    {
       return levels;
