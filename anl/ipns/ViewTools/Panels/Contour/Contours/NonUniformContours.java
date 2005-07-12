65faf28ca31205636a07ea07fedc773e894a79ef
kramer <kramer@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2005-07-12 16:33:14 +0000

    Reorganized the code, added comments, and javadoc statements. Implemented the methods for the IPreserveState interface (which the superclass (Contours) implements directly).

--- anl/ipns/ViewTools/Panels/Contour/Contours/NonUniformContours.java
+++ anl/ipns/ViewTools/Panels/Contour/Contours/NonUniformContours.java
@@ -33,7 +33,13 @@
  *
  * Modified:
  * $Log$
+ * Revision 1.8  2005/07/12 16:33:14  kramer
+ * Reorganized the code, added comments, and javadoc statements.
+ * Implemented the methods for the IPreserveState interface (which the
+ * superclass (Contours) implements directly).
+ *
  * Revision 1.7  2005/06/28 18:55:37  kramer
+ *
  * Because Float.MIN_VALUE is the smallest positive float, the
  * getHighestLevel() would sometimes return incorrect results.  This was
  * fixed.
@@ -60,13 +66,56 @@
  */
 package gov.anl.ipns.ViewTools.Panels.Contour.Contours;
 
+import gov.anl.ipns.ViewTools.Components.ObjectState;
+
 /**
  * Represents a set of contour levels of any size and arrangement.
  */
 public class NonUniformContours extends Contours
 {
+//--------------------------=[ ObjectState keys ]=----------------------------//
+   /**
+    * "Levels" - This static constant String is a key for referencing 
+    * the array of contour levels encapsulated by this class.  The 
+    * value that this key references is a float array.
+    */
+   public static final String CONTOUR_LEVELS_KEY = "Levels";
+//-----------------------=[ End objectState keys ]=---------------------------//
+   
+//------------------------=[ Default field values ]=--------------------------//
+   /**
+    * The default array of contour levels that this object 
+    * encapsulates.  The value of the <code>ith</code> element 
+    * of this array is <code>i</code> and there are 
+    * {@link Contours#DEFAULT_NUM_LEVELS Contours.DEFAULT_NUM_LEVELS} 
+    * elements in the array.
+    */
+   public static final float[] DEFAULT_CONTOUR_LEVELS;
+   /**
+    * Initializes the default array of contour levels that this 
+    * object encapsulates.
+    */
+   static
+   {
+      DEFAULT_CONTOUR_LEVELS = new float[DEFAULT_NUM_LEVELS];
+      for (int i=0; i<DEFAULT_CONTOUR_LEVELS.length; i++)
+         DEFAULT_CONTOUR_LEVELS[i] = i;
+   }
+//----------------------=[ End default field values ]=------------------------//
+   
+//------------------------------=[ Fields ]=----------------------------------//
    /** Holds the value of each contour level. */
    private float[] levels;
+//-----------------------------=[ End fields ]=-------------------------------//
+   
+//---------------------------=[ Constructors ]=-------------------------------//
+   /**
+    * Creates a default set of nonuniformly spaced contour levels.
+    */
+   public NonUniformContours()
+   {
+      this(DEFAULT_CONTOUR_LEVELS);
+   }
    
    /**
     * Create a set of any number and arrangement of contour levels.
@@ -84,7 +133,68 @@ public class NonUniformContours extends Contours
                                             "manually entered");
       this.levels = levels;
    }
+//--------------------------=[ End constructors ]=----------------------------//
 
+//-------------------------=[ Overriden methods ]=----------------------------//
+   /**
+    * Used to get the state information for this collection of contour 
+    * levels.  This method is overriden to include the float array of 
+    * contours in the state information.
+    * 
+    * @param isDefault If true, the default state for this collection of 
+    *                  contour levels is returned.
+    *                  If false, the current state is returned.
+    * @return The state for this collection of contour levels.
+    * @see Contours#getObjectState(boolean)
+    */
+   public ObjectState getObjectState(boolean isDefault)
+   {
+      System.out.println("NonUniformContours:  inside getObjectState()");
+      
+      //get the superclass's state
+      ObjectState state = super.getObjectState(isDefault);
+        //store the contour levels
+        if (isDefault)
+           state.insert(CONTOUR_LEVELS_KEY, DEFAULT_CONTOUR_LEVELS);
+        else
+        {
+           float[] copy = new float[levels.length];
+           System.arraycopy(levels, 0, copy, 0, copy.length);
+           state.insert(CONTOUR_LEVELS_KEY, copy);
+        }
+      return state;
+   }
+   
+   /**
+    * Used to set the state information for this collection of contour 
+    * levels.  This method is overriden to allow the float array of 
+    * contours to be stored in the state.
+    * 
+    * @param state An encapsulation of the state information for this 
+    *              collection of contour levels.
+    * @see Contours#setObjectState(ObjectState)
+    */
+   public void setObjectState(ObjectState state)
+   {
+      System.out.println("NonUniformContours:  inside setObjectState()");
+      
+      //if the state is 'null' do nothing
+      if (state==null)
+         return;
+      
+      //set the state information that the superclass maintains
+      super.setObjectState(state);
+      
+      //set the contour levels
+      Object val = state.get(CONTOUR_LEVELS_KEY);
+      if ( (val != null) && (val instanceof float[]) )
+         this.levels = (float[])val;
+      
+      System.out.println("  set the levels to:");
+      for (int i=0; i<this.levels.length; i++)
+         System.out.println("    levels["+i+"] = "+this.levels[i]);
+   }
+   
    /**
     * Get the value of the <code>ith</code> contour level.
     * 
@@ -101,17 +211,6 @@ public class NonUniformContours extends Contours
          return levels[i];
       else
          return Float.NaN;
-   }
-   
-   /**
-    * Used to get a reference to all of the contour levels.  Note:  The array 
-    * returned is not sorted in any way.
-    * 
-    * @return All of the contour levels in this set.
-    */
-   public float[] getLevels()
-   {
-      return levels;
    }
    
    /**
@@ -141,4 +240,31 @@ public class NonUniformContours extends Contours
             max = levels[i];
       return max;
    }
+//-----------------------=[ End overriden methods ]=--------------------------//
+   
+//-----------------------=[ Convience methods ]=------------------------------//
+   /**
+    * Used to create a copy of this collection of nonuniformly spaced 
+    * contour levels.
+    * 
+    * @return A deep copy of this collection of contour levels.
+    */
+   public NonUniformContours getCopy()
+   {
+      NonUniformContours copy = new NonUniformContours(getLevels());
+      copy.setObjectState(this.getObjectState(false));
+      return copy;
+   }
+   
+   /**
+    * Used to get a reference to all of the contour levels.  Note:  The array 
+    * returned is not sorted in any way.
+    * 
+    * @return All of the contour levels in this set.
+    */
+   public float[] getLevels()
+   {
+      return levels;
+   }
+//---------------------=[ End convience methods ]=----------------------------//
 }
