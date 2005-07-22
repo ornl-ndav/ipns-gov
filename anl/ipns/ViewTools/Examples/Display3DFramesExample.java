9c39cc2c55cd5d8e6ce1074185695a8c85be9d86
cjones <cjones@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2005-07-22 19:48:40 +0000

    Updated Display3DExample with better test data. Also, added a Display3DframesExample to show how to use Display3D when data has frames.

--- anl/ipns/ViewTools/Examples/Display3DFramesExample.java
+++ anl/ipns/ViewTools/Examples/Display3DFramesExample.java
@@ -1,5 +1,5 @@
 /*
- * File:  Display3DExample.java
+ * File:  Display3DFramesExample.java
  *
  * Copyright (C) 2005, Chad Jones
  *
@@ -33,13 +33,11 @@
  *  Modified:
  *
  *  $Log$
- *  Revision 1.2  2005/07/22 19:48:39  cjones
+ *  Revision 1.1  2005/07/22 19:48:40  cjones
  *  Updated Display3DExample with better test data. Also, added a
  *  Display3DframesExample to show how to use Display3D when data has
  *  frames.
  *
- *  Revision 1.1  2005/07/19 15:57:10  cjones
- *  Example file for Display3D.
  * 
  */
  
@@ -49,68 +47,60 @@ package gov.anl.ipns.ViewTools.Examples;
 import gov.anl.ipns.MathTools.Geometry.Vector3D;
 
 import gov.anl.ipns.ViewTools.Displays.Display3D;
-import gov.anl.ipns.ViewTools.Components.IPhysicalArray3D;
-import gov.anl.ipns.ViewTools.Components.PhysicalArray3D;
+import gov.anl.ipns.ViewTools.Components.IPhysicalArray3DList;
+import gov.anl.ipns.ViewTools.Components.PhysicalArray3DList;
 import gov.anl.ipns.Util.Sys.WindowShower;
 
 /**
- * This class is a simple example of how to use the Display3D class.
+ * This class is a simple example of how to use the Display3D class 
+ * with frames.
  */
-public class Display3DExample
+public class Display3DFramesExample
 {
  /*
   * Main program, this is where the work is done.
   */
   public static void main( String args[] )
   {
-    int detectorsize = 100;
-    int numdetectors = 3;
+  	int detectorsize = 100;
+  	int numdetectors = 2;
+  	
+  	IPhysicalArray3DList[] data = new IPhysicalArray3DList[numdetectors];
 
-    // Example object that points data into an IPhysicalArray3D
-    IPhysicalArray3D[] data = new IPhysicalArray3D[numdetectors];
-
-    // Create 3 square panels
+    // Create 2 square panels
     // PANEL 1
-    data[0] = new PhysicalArray3D(detectorsize);
+  	float[] time_vals = {1.5f, 2.6f, 3.6f, 5.2f };
+  	DataSetTools.dataset.XScale time = new DataSetTools.dataset.VariableXScale(time_vals);
+  	
+    data[0] = new PhysicalArray3DList(detectorsize, time);
     for(int i = 0; i < 10; i++)
       for(int j = 0; j < 10; j++)
       {
+      	float[] vals = { j*30+i*10-150, j*30+i*10-2*150, j*30+i*10-3*150, j*30+i*10-4*150 }; 
       	data[0].set(i*10+j, // Index
-      			  new Vector3D(i*50, -100, j*50), // 3D Coordinates
+      			  new Vector3D(i*50, 400, j*50), // 3D Coordinates
                   new Vector3D(11, 13, 12),  // Box volume
                   new Vector3D(1, 0, 0), // Orientation (X-Axis)
 				  new Vector3D(0, 1, 0), // Orientation (Y-Axis)
-				  j*30+i*10-150); // Value
+				  vals); // Value
       }
   	data[0].setArrayID(0);  // Panel ID
     
     // PANEL 2
-    data[1] = new PhysicalArray3D(detectorsize);
+    data[1] = new PhysicalArray3DList(detectorsize, time);
     for(int i = 0; i < 10; i++)
       for(int j = 0; j < 10; j++)
       {
-        data[1].set(i*10+j, // Index 
-        		  new Vector3D(i*50, 600, j*50), // 3D Coordinates
-                  new Vector3D(13, 9, 12), // Box Volume
-                  new Vector3D(-1, 2, 5), // Orientation (X-Axis)
-				  new Vector3D(3, 4, -1), // Orientation (Y-Axis
-				  j*30+i*10+300); // Value
-      }
-    data[1].setArrayID(1); // Panel ID
-    
-    // PANEL 3
-    data[2] = new PhysicalArray3D(detectorsize);
-    for(int i = 0; i < 10; i++)
-      for(int j = 0; j < 10; j++)
-      {
-        data[2].set(i*10+j, // Index
-			      new Vector3D(i*50, j*50, -100), // 3D Coordinates
+      	float[] vals = { j*30+i*10+750, j*30+i*10+2*750, j*30+i*10+3*750, j*30+i*10+4*750 }; 
+      	
+        data[1].set(i*10+j, // Index
+			      new Vector3D(i*50, -400, j*50), // 3D Coordinates
                   new Vector3D(9, 10, 9), // Box Volume
                   new Vector3D(4, 0, -2), // Orientation (X-Axis)
 				  new Vector3D(1, 1, 2),  // Orientation (Y-Axis)
-				  j*30+i*10+750); // Value
+				  vals); // Value
       }
-    data[2].setArrayID(2); // Panel ID
+    data[1].setArrayID(1); // Panel ID
     
     // Create display with Controls
     Display3D display = new Display3D(data, 0, Display3D.CTRL_ALL);
