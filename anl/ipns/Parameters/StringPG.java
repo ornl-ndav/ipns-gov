532c82c5b8daa35188aa124adc99284832260cf5
Dennis Mikkelson <mikkelsond@uwstout.edu>
2006-06-29 20:13:21 +0000

    Modified to do virtually all of the work in the new base class FilteredStringPG.  Now just extends FilteredStringPG and uses the AllPassFilter as it's filter, so that any String is accepted.

--- anl/ipns/Parameters/StringPG.java
+++ anl/ipns/Parameters/StringPG.java
@@ -32,6 +32,11 @@
  * Modified:
  *
  *  $Log$
+ *  Revision 1.2  2006/06/29 20:13:21  dennis
+ *  Modified to do virtually all of the work in the new base class
+ *  FilteredStringPG.  Now just extends FilteredStringPG and uses
+ *  the AllPassFilter as it's filter, so that any String is accepted.
+ *
  *  Revision 1.1  2006/06/23 14:14:45  dennis
  *  Concrete class for entering a string from a TextField.
  *
@@ -45,18 +50,12 @@ import javax.swing.*;
 /**
  *  A StringPG uses a JTextField component to let the user specify a String.
  */
-public class StringPG extends StringPG_base
+public class StringPG extends FilteredStringPG
 {
-  private JPanel     panel      = null;
-  private JTextField text_field = null;
-  private JLabel     label      = null;
-  private boolean    enabled    = true;  // we store the enabled state, so the
-                                         // setEnabled() method can be called
-                                         // before constructing the widget.
 
   /**
    * Creates a new StringPG object with the specified name and initial
-   * value.
+   * value.  A StringPG will let the user enter an arbitrary String.
    *
    * @param  name  The name (i.e. prompt string) for this PG.
    * @param  val   The initial value for this PG.
@@ -66,126 +65,7 @@ public class StringPG extends StringPG_base
    */
   public StringPG( String name, Object val ) throws IllegalArgumentException
   {
-    super( name, Conversions.get_String( val ) );  
-                          // NOTE: The construction of the GUI with label
-                          //       and widget is done on demand, so it does
-                          //       NOT get done in the constructor.  We just
-                          //       need to call the super class constructor
-                          //       to record the name and default value.
-  }
-
-  
-  /**
-   * Construct a copy of this StringPG object.
-   *
-   * @return A copy of this StringPG, with the same name and value.
-   */
-  public Object getCopy() 
-  {
-     StringPG copy = new StringPG( getName(), str_value );
-     copy.setValidFlag( getValidFlag() );
-     return copy;
-  }
-
-  
-   /**
-   * Get a JPanel containing a composite entry widget for getting a value from
-   * the user.  In this case, the panel contains two children, a label 
-   * giving the name (i.e. prompt string) and a JTextField. 
-   * The JTextField's value and enabled state is set from the currently
-   * recorded values. 
-   *
-   * @return a JPanel containing a component to enter a value.
-   */
-  protected JPanel getWidget() 
-  {
-    if( panel == null )                // make new panel with label & TextField 
-    {
-      panel      = new JPanel( new GridLayout( 1, 2 ) );
-      text_field = new JTextField();
-      label      = new JLabel( getName() );
-
-      panel.add( label );
-      panel.add( text_field );
- 
-      text_field.addActionListener( new PG_ActionListener( this ) );
-      text_field.addKeyListener( new PG_KeyListener( this ) );
-    }
-
-    setEnabled( enabled );                 // set widget state from
-    setWidgetValue( str_value );           // current information
-
-    return panel;
-  }
-
-
-  /**
-   * Set all internal references to the JPanel and entry components to NULL,
-   * so that it can be garbage collected.  Subsequent calls to getPGWidget()
-   * will return a new JPanel and entry components.
-   */
-  protected void destroyWidget()
-  {
-    panel      = null;                     // null out all references to gui 
-    text_field = null;                     // components, so that they can be
-    label      = null;                     // garbage collected.
-  }
-
-
-  /**
-   * Enable or disable the JTextField for entering values. 
-   *
-   * @param  on_off  Set true to enable the JTextField for user input.
-   */
-  public void setEnabled( boolean on_off )
-  {
-    enabled = on_off;
-    if ( panel != null )                // panel, box and label are created and
-    {                                   // destroyed together, so we can just 
-      text_field.setEnabled( on_off );  // check that the panel is there
-      label.setEnabled( on_off );
-    }
-  }
-
-
-  /**
-   * Retrieves the JTextField's current value.  
-   * 
-   * @return The String value from the JTextField, if possible.
-   *
-   * @throws IllegalArgumentException is thrown if this is called without
-   *         a GUI widget being present, or if the value is invalid.
-   */
-  protected String getWidgetValue() throws IllegalArgumentException
-  {
-    String widget_value = "";
-
-    if ( text_field == null )
-      throw new IllegalArgumentException(
-              "getWidgetValue() called when no StringPG widget exists");
-
-    else
-      widget_value = Conversions.get_String( text_field.getText() );      
-
-    return widget_value;
-  }
-
-
-  /**
-   * Sets the value displayed in the JTextField to the specified value.
-   *
-   * @param value  The String value to record in the JTextField.
-   *
-   * @throws IllegalArgumentException is thrown if this is called without
-   *         a GUI widget being present.
-   */
-  protected void setWidgetValue( String value ) throws IllegalArgumentException
-  {
-    if ( text_field == null )
-      throw new IllegalArgumentException(
-              "setWidgetValue() called when no BooleanPG widget exists");
-
-    text_field.setText( ""+value );
+    super( name, Conversions.get_String( val ), new AllPassFilter() );  
   }
 
 }
