6c20952c0e76171a427f059b06b6944af24205af
oakgrovej <oakgrovej@cd0dc835-f711-0410-98b2-fd585fc23d7c>
2007-07-25 22:26:00 +0000

    Added Hashtables and logic for setting Attributes: orientation and number of pages are the only ones currently.

--- anl/ipns/DisplayDevices/PrinterDevice.java
+++ anl/ipns/DisplayDevices/PrinterDevice.java
@@ -1,5 +1,8 @@
 /*
  * $Log$
+ * Revision 1.6  2007/07/25 22:26:00  oakgrovej
+ * Added Hashtables and logic for setting Attributes: orientation and number of pages are the only ones currently.
+ *
  * Revision 1.5  2007/07/17 16:17:05  oakgrovej
  * Added Throws Exception where needed
  *
@@ -25,21 +28,43 @@ package gov.anl.ipns.DisplayDevices;
 import gov.anl.ipns.DisplayDevices.IDisplayable;
 import gov.anl.ipns.DisplayDevices.VirtualArray2D_Displayable;
 import gov.anl.ipns.Util.File.ImageRenderWriter;
+import gov.anl.ipns.Util.Sys.PrintUtilities2;
 import gov.anl.ipns.ViewTools.Components.VirtualArray2D;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.util.Hashtable;
 import java.util.Vector;
 
+import javax.print.attribute.Attribute;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.standard.Copies;
+import javax.print.attribute.standard.OrientationRequested;
 import javax.swing.JComponent;
 
 public class PrinterDevice extends GraphicsDevice
 {
+  public static String LANDSCAPE = "landscape";
+  public static String Portraite = "landscape";
+  
+  private Hashtable<String, Attribute> attributes = 
+    new Hashtable<String, Attribute>();
+  private Hashtable<String, Object> values = new Hashtable<String, Object>();
+  
+  protected HashPrintRequestAttributeSet aset;
+  
+  private JComponent jcomp;
   private String printer_name;
-  private BufferedImage bimg = null;
+  private PrintUtilities2 prinUtil;
   
   public PrinterDevice(String printer_name)
   {
     this.printer_name = printer_name;
+    aset = new HashPrintRequestAttributeSet();
+    buildAttributes();
+    buildValues();
+    width = 500;
+    height = 500;
   }
   
   /**
@@ -49,10 +74,24 @@ public class PrinterDevice extends GraphicsDevice
    * @param value - Value for the attribute
    */
   @Override
-  public void setDeviceAttribute(String name, Object value) 
+  public void setDeviceAttribute(String name, Object value) throws Exception
   {
-    // TODO Auto-generated method stub
+    name = name.toLowerCase();
     
+    if( name.equals("orientation") && value instanceof String)
+    {
+      value = ((String)value).toLowerCase();
+      name += (String)Util.TranslateKey( values, (String)value );
+    }
+    
+    Attribute attrib = (Attribute)Util.TranslateKey(attributes, name);
+    
+    if( attrib instanceof Copies && value instanceof Integer )
+      attrib = new Copies((Integer)value);
+    
+    System.out.println("setting attribute:\t"+attrib+":     \t"
+        +attrib.getClass());
+    aset.add(attrib);
   }
   
   /**
@@ -62,8 +101,15 @@ public class PrinterDevice extends GraphicsDevice
   @Override
   public void print() 
   {
-    // TODO Auto-generated method stub
-    
+    //System.out.println("PrinterDevice.print()");
+    Attribute[] array = aset.toArray();
+   /* for( int i = 0; i <array.length; i++ )
+    {
+      System.out.println(array[i]+":\t"+array[i].getClass());
+    }*/
+    //prinUtil = new PrinterUtilities2(aset);
+    prinUtil = new PrintUtilities2(jcomp, printer_name, aset);
+    prinUtil.print();
   }
 
   /**
@@ -113,13 +159,26 @@ public class PrinterDevice extends GraphicsDevice
   @Override
   public void display(JComponent jcomp) 
   {
-    jcomp.setSize( (int)width, (int)height );
-    bimg = ImageRenderWriter.render(jcomp);  
+    this.jcomp = jcomp;
+    this.jcomp.setSize( (int)width, (int)height );  
   }
   
-  public static void main(String[] args)throws Exception
+  private void buildAttributes()
   {
-    String type = "ImageV2D";
+    attributes.put("orientation.portrait", OrientationRequested.PORTRAIT);
+    attributes.put("orientation.landscape", OrientationRequested.LANDSCAPE);
+    attributes.put("copies", new Copies(1));
+  }
+  
+  private void buildValues()
+  {
+    values.put("portrait", ".portrait");
+    values.put("landscape", ".landscape");
+  }
+  
+  /*public static void main(String[] args)throws Exception
+  {
+    String type = "Image";
     VirtualArray2D v2d = new VirtualArray2D( 
              new float[][]{
                       { 1,1,1,1,1,1,1,1,1 },
@@ -129,18 +188,23 @@ public class PrinterDevice extends GraphicsDevice
                       { 5,5,5,5,5,5,5,5,5 },
                       { 6,6,6,6,6,6,6,6,6 }
                       
-             });
-    VirtualArray2D_Displayable va2d_disp =  
-                               new VirtualArray2D_Displayable( v2d, type);
+             });//
+    //VirtualArray2D_Displayable va2d_disp =  
+    //                           new VirtualArray2D_Displayable( v2d, type);
     
-    va2d_disp.setViewAttribute("ColorModel", "Rainbow");
-    va2d_disp.setViewAttribute("Axes Displayed", new Integer(2));
+    //va2d_disp.setViewAttribute("ColorModel", "Rainbow");
+    //va2d_disp.setViewAttribute("Axes Displayed", new Integer(2));
     
     PrinterDevice pr_dev = new PrinterDevice("");
-    pr_dev.setRegion(50,50,650,550);
-    
-    pr_dev.display(va2d_disp,false);
     pr_dev.print();
-  }
+    pr_dev.setDeviceAttribute("orientation", "portrait");
+    pr_dev.setDeviceAttribute("orientation", "landscape");
+    pr_dev.setDeviceAttribute("copies", 23);
+    pr_dev.print();
+    ///pr_dev.setRegion(50,50,650,550);
+    
+    //pr_dev.display(va2d_disp,false);
+    //pr_dev.print();
+  }//*/
 
 }
