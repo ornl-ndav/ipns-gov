b0b10d33fa6b56554dc65e7ef93389c8fb16162b
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2007-08-22 15:15:06 +0000

    Added GPL

--- anl/ipns/DisplayDevices/PreviewDevice.java
+++ anl/ipns/DisplayDevices/PreviewDevice.java
@@ -1,5 +1,38 @@
 /*
+  * File: PreviewDevice.java 
+ *  
+ * Copyright (C) 2007     Andy Moe
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact :  Dennis Mikkelson<mikkelsond@uwstout.edu>
+ *            MSCS Department
+ *            Menomonie, WI. 54751
+ *            (715)-232-2291
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0426797, and by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ *
+ * Modified:
+ * 
  * $Log$
+ * Revision 1.9  2007/08/22 15:13:28  rmikk
+ * Added GPL
+ *
  * Revision 1.8  2007/08/20 17:38:25  amoe
  * The following changes were made to undo part of the previous change.
  * -Removed image-to-frame ratio.
--- anl/ipns/DisplayDevices/ScreenDevice.java
+++ anl/ipns/DisplayDevices/ScreenDevice.java
@@ -1,5 +1,37 @@
 /*
+ * File: PreviewDevice.java 
+ *  
+ * Copyright (C) 2007     Andy Moe
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact :  Dennis Mikkelson<mikkelsond@uwstout.edu>
+ *            MSCS Department
+ *            Menomonie, WI. 54751
+ *            (715)-232-2291
+ *
+ * This work was supported by the National Science Foundation under grant
+ * number DMR-0426797, and by the Intense Pulsed Neutron Source Division
+ * of Argonne National Laboratory, Argonne, IL 60439-4845, USA.
+ *
+ *
+ * Modified:
  * $Log$
+ * Revision 1.8  2007/08/22 15:14:36  rmikk
+ * Added GPL
+ *
  * Revision 1.7  2007/07/31 19:00:55  amoe
  * -Added static final default frame width and height.
  * -Set the frame to the default size when the current size has not been set (when it's -1).
@@ -41,6 +73,7 @@ import javax.swing.JFrame;
 import gov.anl.ipns.DisplayDevices.IDisplayable;
 import gov.anl.ipns.DisplayDevices.VirtualArray2D_Displayable;
 import gov.anl.ipns.ViewTools.Components.VirtualArray2D;
+import gov.anl.ipns.Util.Sys.*;
 
 public class ScreenDevice extends GraphicsDevice
 {
@@ -120,6 +153,8 @@ public class ScreenDevice extends GraphicsDevice
     JComponent jcomp = disp.getJComponent( with_controls );
 
     display(jcomp);
+    
+   
   }
 
   /**
