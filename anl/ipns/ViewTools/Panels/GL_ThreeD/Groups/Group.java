1b085c2129ca81a3fee488576c3492b156a16c08
Dennis Mikkelson <mikkelsond@uwstout.edu>
2008-06-18 18:59:53 +0000

    Added method clearList() that calls clearList() on all of the shapes in this group.

--- anl/ipns/ViewTools/Panels/GL_ThreeD/Groups/Group.java
+++ anl/ipns/ViewTools/Panels/GL_ThreeD/Groups/Group.java
@@ -95,6 +95,26 @@ public class Group implements IThreeD_GL_Object
       return (IThreeD_GL_Object)objects.elementAt( index );
   }
 
+
+  /* ---------------------------- clearList ------------------------- */
+  /**
+   *  Delete the display list and reset the list_id to invalid.  This
+   *  will force the display list to be regenerated if this node is
+   *  rendered again.  This method should be called when this shape is
+   *  no longer needed.  Although the finalize method will free the
+   *  cause the display list to be deleted, the garbage collector may
+   *  not run for quite some time, and the OpenGL display list would
+   *  not be freed until the garbage collector runs.  To reduce the
+   *  number of stale display lists maintained by OpenGL, call this 
+   *  method, as soon as the shape is no longer needed.
+   */
+  public void clearList()
+  {
+    for ( int i = 0; i < objects.size(); i++ )
+      ((IThreeD_GL_Object)objects.elementAt(i)).clearList();
+  }
+
+
   /**
    *  Render this object using the specified drawable.
    *
