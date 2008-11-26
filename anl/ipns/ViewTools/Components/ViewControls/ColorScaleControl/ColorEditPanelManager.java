576537941a4a4003d064c2e806ca036e9671269a
Dennis Mikkelson <mikkelsond@uwstout.edu>
2008-11-26 04:42:05 +0000

    The doUpdate() method now uses a ColorScaleInfo object.

--- anl/ipns/ViewTools/Components/ViewControls/ColorScaleControl/ColorEditPanelManager.java
+++ anl/ipns/ViewTools/Components/ViewControls/ColorScaleControl/ColorEditPanelManager.java
@@ -1,3 +1,35 @@
+/* 
+ * File: ColorEditPanelManager.java
+ *
+ * Copyright (C) 2008, Dennis Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Dennis Mikkelson <mikkelsond@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ *  Last Modified:
+ * 
+ *  $Author$
+ *  $Date$            
+ *  $Revision$
+ */
+
+
 package gov.anl.ipns.ViewTools.Components.ViewControls.ColorScaleControl;
 
 import java.awt.*;
@@ -8,6 +40,7 @@ import javax.swing.border.TitledBorder;
 
 import gov.anl.ipns.ViewTools.Components.ViewControls.*;
 import gov.anl.ipns.ViewTools.Panels.Image.*;
+import gov.anl.ipns.Util.Sys.*;
 
 /**
  *  This class coordinates the color scale editor and an 
@@ -61,19 +94,38 @@ public class ColorEditPanelManager extends ViewControl
   }
 
 
+  /**
+   *  Get the ColorEditPanel information and pass it on to the image 
+   *  display.
+   */
   private void doUpdate()
   {
-    System.out.println("UPDATE IMAGE DISPLAY FROM THE COLOR EDITOR INFO");
-    float min      = color_editor.getLocalMin(); 
-    float max      = color_editor.getLocalMax(); 
-    float prescale = color_editor.getPrescale();
-    String cs_name = color_editor.getColorScale();
-    byte[] table   = color_editor.getColorMapping();
-    boolean isLog  = color_editor.getLogScale();
+    ColorScaleInfo info = null;
 
-    boolean two_sided = false;  // NOTE: In the future, we could allow this.
+    Object value = color_editor.getControlValue();
 
-    ijp.setNamedColorModel( cs_name, two_sided, false );
+    if ( value instanceof ColorScaleInfo )
+      info = (ColorScaleInfo)value;
+    else
+    {
+      SharedMessages.addmsg("Value NOT ColorScaleInfo in " +
+                            " ColorEditPanelManager, class is: " +
+                              value.getClass() );
+      return;
+    }
+
+    float   min        = info.getTableMin(); 
+    float   max        = info.getTableMax(); 
+    float   prescale   = info.getPrescale();
+
+    String  cs_name    = info.getColorScaleName();
+    boolean two_sided  = info.isTwoSided();
+    int     num_colors = info.getNumColors(); 
+
+    byte[]  table      = info.getColorIndexTable();
+    boolean isLog      = info.isLog();
+
+    ijp.setNamedColorModel( cs_name, two_sided, num_colors, false );
 
     ijp.changeColorIndexTable( table,
                                isLog,
@@ -83,23 +135,30 @@ public class ColorEditPanelManager extends ViewControl
   }
 
 
+  /**
+   *  Listener class to pop up the ColorEditPanel window when the user
+   *  presses the Edit button.
+   */
   public class EditButtonListener implements ActionListener
   {
      public void actionPerformed( ActionEvent ae )
      {
-        System.out.println("SHOW COLOR SCALE EDITOR");
         send_message( COLOR_SCALE_CHANGED );
         frame.setVisible( true );
      }
   } 
 
+
+  /**
+   *  Listener class to pop up do the update of the image from the 
+   *  color edit panel information and possibly close the ColorEditPanel
+   *  window if the user presses the Cancel or Done buttons.
+   */
   public class EditorListener implements ActionListener
   {
      public void actionPerformed( ActionEvent ae )
      {
        String message = ae.getActionCommand();
-
-       System.out.println("Editor Update Pushed");
 
        if ( message.equals( ColorEditPanel.doneMessage ) )
        {
