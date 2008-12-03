5d92e42c367f55d6269bf5f53f66620568854838
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2008-12-03 21:18:53 +0000

    Fixed mastervalue when slider is changed to have a value with a small number of digits base 10 in that interval

--- anl/ipns/ViewTools/Components/ViewControls/ColorScaleControl/Stretcher.java
+++ anl/ipns/ViewTools/Components/ViewControls/ColorScaleControl/Stretcher.java
@@ -114,6 +114,7 @@ public class Stretcher extends ActiveJPanel
 		return masterValue;
 	}
 	
+	
 	/**
 	 * Sets the text and slider value.
 	 * 
@@ -121,7 +122,7 @@ public class Stretcher extends ActiveJPanel
 	 */
 	public void setValue(float value)
 	{
-		String str = Float.toString(Round(value,2));
+		String str = "" + value;
 		textField.setText(str);
 	   prevTextFieldValue = str;
 		changeValue(true);
@@ -228,6 +229,184 @@ public class Stretcher extends ActiveJPanel
 		public void keyTyped(KeyEvent key){}
 	};
 	
+	private String padd( String S, int nleadingZeroes){
+	  
+	  
+	  if( S == null || nleadingZeroes <= 0)
+	     return S;
+	  char[] Res = new char[ S.length()+nleadingZeroes];
+	  int k =0;
+	  if( S.startsWith(  "-")){
+	     k = 1;
+	     Res[0]='-';
+	  }
+	  java.util.Arrays.fill(Res,k,nleadingZeroes+k,'0');
+	  System.arraycopy( S.toCharArray() , k ,Res , nleadingZeroes+k ,
+	                                                   S.length()-k );
+	  return new String( Res );
+	   
+	}
+	
+	// st1 < st2  do for digit pos etc. Assumes leading digits one apart
+	// trailing digits 9 and 0 keep going. Assumes both same length, have
+	// decimal points that line up.
+	private String GetNextDigits( String st1, String st2, int posStart){
+	  
+	   if( st1 == null || st2== null 
+	              || posStart < 0)
+	      return "";
+
+	  
+	      String small, large;
+	      if( st1.charAt( posStart )< st2.charAt( posStart )){
+	         small = st1;
+	         large = st2;
+	      }else {
+	         small = st2;
+	         large = st1;
+	      }	
+	      int cc = ((int)small.charAt( posStart )+(int)large.charAt(  posStart ))/2;
+	      if( cc != (int)small.charAt( posStart ) && cc !=(int)large.charAt(  posStart ))
+	         return ""+(char)cc;
+	      if( posStart +1 >= st1.length() )//assumes at end of string with a dot	        
+	         return ""+(char)cc+"5";
+	      String Res="";
+	      int k=1;
+	      if( small.charAt( posStart+k )=='.')k++;
+	      int nextChar1 = (int)small.charAt( posStart+k );
+         int nextChar2 = (int)large.charAt( posStart+k );
+         int nextChar = (nextChar1+nextChar2+10)/2;
+         Res+=""+(char)small.charAt( posStart );
+         if( nextChar - (int)'0' > 9){
+            Res = ""+(char)large.charAt( posStart );
+            if( k >1)
+               Res +=".";
+            Res += (char)(nextChar-10);
+            return Res;
+         }
+         if( nextChar != nextChar1 ){
+            if(k > 1)
+               Res +='.';
+            Res += (char)nextChar;
+            return Res;
+         }
+         
+         // should be at case where n9  and (n+1)0   
+            
+	      return GetNextDig999( small, large, posStart, posStart+2);
+	      
+	 
+	   
+	         
+	   
+	}
+	// small is x9999 and large is y0000  where x is y-1
+	private String GetNextDig999( String small, String large, int posStart,
+	              int currentPos){
+	   
+	   if( currentPos >= small.length())
+	      return small.substring( posStart);
+	  if( small.charAt( currentPos )!='9' || large.charAt( currentPos )!='0'){
+	     int nextChar1 = (int)small.charAt( posStart+1 );
+        int nextChar2 = (int)large.charAt( posStart+1 );
+        int nextChar = (nextChar1+nextChar2+10)/2;
+        String Res=small.substring( posStart,currentPos )+(char)nextChar;
+        if( nextChar - (int)'0' > 9){
+           Res =large.substring( posStart, currentPos );
+           Res += (char)(nextChar-10);
+           return Res;
+        }
+        return Res;
+	  }
+	  return GetNextDig999( small, large, posStart, currentPos+1);
+	   
+	}
+	//Assumes decimal point
+	private String paddTrailing( String S, int nZeroes ){
+	   if( nZeroes <=0)
+	      return S;
+	   char[] Res = new char[nZeroes];
+	   java.util.Arrays.fill( Res , '0' );
+	   return S + new String( Res);
+	}
+	// If sliderPos =0 or 99 will give max or min value
+	// otherwise will pick the point in Interval with the fewest number
+	// of significant digits.
+	private float sliderValFixed( int sliderPos, float minInterval, 
+	                                      float maxInterval){
+	   int i;
+	   if( sliderPos ==0)
+	      return minimum;
+	   if( sliderPos == 100)
+	      return maximum;
+	   if( minInterval < 0 && maxInterval > 0)
+	      return 0f;
+	   String dig1 = (new Float(minInterval)).toString();
+      String dig2 = (new Float(maxInterval)).toString();
+      if( dig1 == null || dig2 == null)
+         return 0f;
+      
+      //Make sure decimal points line up
+      int posDecimal_min = dig1.indexOf( '.' );
+      int posDecimal_max = dig2.indexOf( '.' );
+      if( posDecimal_min < 0){
+         posDecimal_min = dig1.length(); 
+         dig1 +=".";
+      }
+      if( posDecimal_max < 0){
+         posDecimal_max = dig2.length(); 
+         dig2 +=".";
+      }
+      if( posDecimal_min < posDecimal_max){
+         dig1 = padd( dig1, posDecimal_max -posDecimal_min);
+         posDecimal_min = posDecimal_max;
+      } else if( posDecimal_max < posDecimal_min){
+         dig2 = padd( dig2, posDecimal_min -posDecimal_max);
+         posDecimal_max = posDecimal_min;
+      }
+      dig1 = paddTrailing( dig1, dig2.length()-dig1.length());
+      dig2 = paddTrailing( dig2, dig1.length()-dig2.length());
+      
+      for(  i=0; i < Math.min( dig1.length(), dig2.length()) && 
+                   dig1.charAt(i) == dig2.charAt(i);i++){}
+     
+      if( i >=dig1.length() && i >= dig2.length())
+         return minInterval;
+      //if( minInterval < 0) i--;
+      String S = dig1.substring(0,i);
+      char c_min = ' ',
+           c_max = ' ';
+      if( i < dig1.length())
+         c_min = dig1.charAt( i );
+      
+      
+      if( i < dig2.length())
+         c_max = dig2.charAt( i );
+    
+      String Res = S;
+      int c_int =  ((int)c_min +(int)c_max)/2;
+      if( c_int != (int) c_max && c_int != (int)c_min)
+         Res += (char)c_int;
+     
+      else{
+         
+         Res += GetNextDigits( dig1,dig2,i);
+         
+         
+      }
+      
+      if( Res.length() <  posDecimal_min )
+         Res = paddTrailing( Res, posDecimal_min - Res.length());
+         
+      
+      return (new Float(Res)).floatValue();
+      
+      
+      
+	   
+	   
+	}
+	
 	/** The MouseListener to control input from the mouse for the slider. */
 	MouseListener mouse = new MouseListener()
 	{
@@ -293,9 +472,11 @@ public class Stretcher extends ActiveJPanel
 		}
 		else
 		{
-			float value = slider.getValue();
-			masterValue = (value*((Math.max(maximum,minimum)-Math.min(minimum,maximum))/100))+Math.min(minimum,maximum);
-			String str = Float.toString(Round(masterValue, 2));
+			int value = slider.getValue();
+			float delta = ((Math.max(maximum,minimum)-Math.min(minimum,maximum))/100);
+			masterValue = value*delta+Math.min(minimum,maximum);
+			masterValue = sliderValFixed( value, masterValue, masterValue+delta);
+			String str = Float.toString(masterValue);
 			textField.setText(str);
 			prevTextFieldValue = str;
 		}
