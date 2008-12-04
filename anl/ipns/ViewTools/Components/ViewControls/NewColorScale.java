c9f38866404bac7d143bbc73b0da7dbda8a07f4a
Dennis Mikkelson <mikkelsond@uwstout.edu>
2008-12-04 18:59:32 +0000

    Added javadocs and a "nearly" default constructor that provides a default value for the info object, and just requires a title.

--- anl/ipns/ViewTools/Components/ViewControls/NewColorScale.java
+++ anl/ipns/ViewTools/Components/ViewControls/NewColorScale.java
@@ -25,35 +25,99 @@ public class NewColorScale extends ViewControl
   private JPanel         scale_panel;
 
 
+  /**
+   *  Construct a color scale with default values.
+   *
+   *  @param title  The title to appear on the border of the color scale.
+   */
+  public NewColorScale( String title )
+  {
+    super( title );
+
+    Axis.Orientation orientation = Axis.Orientation.HORIZONTAL;
+    float   max        = 100;
+    float   min        = 0;
+    float   prescale   = 1;
+    String  cs_name    = IndexColorMaker.RAINBOW_SCALE;
+    boolean two_sided  = false;
+    int     num_colors = 20;
+    byte[]  table      = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
+    boolean is_log     = false;
+
+    info = new ColorScaleInfo( orientation,
+                               min,
+                               max,
+                               prescale,
+                               cs_name,
+                               two_sided,
+                               num_colors,
+                               table,
+                               is_log  );
+    buildPanel();
+    resetCalibrations();
+  }
+
+  /**
+   *  Construct a color scale with values specified by a ColorScaleInfo
+   *  object.
+   *
+   *  @param info   The bundle of information describing this color scale.
+   *  @param title  The title to appear on the border of the color scale.
+   */
   public NewColorScale( String title, ColorScaleInfo info )
   {
     super( title );
     this.info = info;
 
-    build_panel();
+    buildPanel();
+    resetCalibrations();
   }
 
+  /**
+   *  Set the "value" of this control.  The object MUST be ColorScaleInfo
+   *  object, or an IllegalArgumentException will be thrown.
+   *
+   *  @param value  The ColorScaleInfo object that holds all of the information
+   *                that constitutes the value of this color scale.
+   */
   public void setControlValue(Object value)
   {
     if ( !(value instanceof ColorScaleInfo) )
       throw new IllegalArgumentException("Need ColorScaleInfo parameter");
 
     this.info = (ColorScaleInfo)value;
-    build_panel();
+    buildPanel();
+    resetCalibrations();
   }
   
+
+  /**
+   *  Get the "value" of this control.  The object returned will be a
+   *  ColorScaleInfo object.
+   *
+   *  @return  The ColorScaleInfo object that holds all of the information
+   *           that constitutes the value of this color scale.
+   */
   public Object getControlValue()
   {
     return info;
   }
   
+ 
+  /** 
+   *  Make a new NewColorScale control that has the same title and 
+   *  information as this one.
+   */
   public ViewControl copy()
   {
     return new NewColorScale( getTitle(), info );
   }
 
 
-  private void build_panel()
+  /**
+   *
+   */
+  private void buildPanel()
   {
     image_panel = new ImageJPanel2();
     scale_panel = new JPanel();
@@ -74,7 +138,18 @@ public class NewColorScale extends ViewControl
     addComponentListener( new RedrawListener() );
 
     VirtualArray2D va2D = buildImageArray( NUM_VALUES );
-    image_panel.setData( va2D, true );
+    image_panel.setData( va2D, false );
+    image_panel.setDataRange( info.getTableMin(), info.getTableMax() );
+    image_panel.changeColorIndexTable( info.getColorIndexTable(),
+                                       info.isLog(),
+                                       info.getTableMin(),
+                                       info.getTableMax(),
+                                       false );
+  
+    image_panel.setNamedColorModel( info.getColorScaleName(),
+                                    info.isTwoSided(),
+                                    info.getNumColors(),
+                                    true );
   }
 
   private void resetCalibrations()
@@ -165,28 +240,16 @@ public class NewColorScale extends ViewControl
 
   public static void main( String args[] )
   {
-    Axis.Orientation orientation;
-/*
-    orientation = Axis.Orientation.HORIZONTAL;
-*/
-    orientation = Axis.Orientation.VERTICAL;
+    Axis.Orientation orientation = Axis.Orientation.VERTICAL;
 
     float   prescale   = 1;
     String  cs_name    = IndexColorMaker.RAINBOW_SCALE;
     boolean two_sided  = false;
     int     num_colors = 20;
     byte[]  table      = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
-
-/*
-    float   max        = 100;
-    float   min        = 0;
-    boolean is_log     = false;
-*/
-
     float   max        = (float)1e-4;
     float   min        = (float)1e-6;
     boolean is_log     = true;
-   
 
     ColorScaleInfo info = new ColorScaleInfo( orientation,
                                               min,
@@ -199,6 +262,8 @@ public class NewColorScale extends ViewControl
                                               is_log  );
 
     NewColorScale scale = new NewColorScale( "My Scale", info );
+
+//    NewColorScale scale = new NewColorScale( "My Scale" );
  
     JFrame frame = new JFrame( "NewColorScale" );
     frame.getContentPane().setLayout( new GridLayout(1,1) );
