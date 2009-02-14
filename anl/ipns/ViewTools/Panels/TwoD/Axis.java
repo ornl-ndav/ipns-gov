f12bd394183a4387136e9c9cd3a9cd2dc62066c8
Dennis Mikkelson <mikkelsond@uwstout.edu>
2009-02-14 21:14:14 +0000

    Made enum type static to keep the jdk 1.6.0_12 compiler happy.

--- anl/ipns/ViewTools/Panels/TwoD/Axis.java
+++ anl/ipns/ViewTools/Panels/TwoD/Axis.java
@@ -40,7 +40,7 @@ import java.awt.*;
  */
 public abstract class Axis extends Drawable
 {
-  public static enum  Orientation { HORIZONTAL, VERTICAL };
+  public enum  Orientation { HORIZONTAL, VERTICAL };
 
   protected Orientation orientation;
 
