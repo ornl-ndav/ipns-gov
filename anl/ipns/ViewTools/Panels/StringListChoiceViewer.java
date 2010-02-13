b26c2d4a3cd388e79112df083e59c0a329d67a92
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2010-02-13 19:12:42 +0000

    Added javadocs and GPL
    Added option to not show the select button
    
    Set the font to a monospaced font

--- anl/ipns/ViewTools/Panels/StringListChoiceViewer.java
+++ anl/ipns/ViewTools/Panels/StringListChoiceViewer.java
@@ -1,3 +1,36 @@
+/* 
+ * File: StringListChoiceViewer.java
+ *
+ * Copyright (C) 2010, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Ruth Mikkelson <mikkelsonr@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge, TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author$:
+ *  $Date$:            
+ *  $Rev$:
+ */
 package gov.anl.ipns.ViewTools.Panels;
 
 import javax.swing.*;
@@ -7,75 +40,143 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class StringListChoiceViewer extends JPanel  implements ChangeListener,
-                                                               ActionListener
+/**
+ * 
+ * @author ruth
+ * 
+ */
+public class StringListChoiceViewer extends JPanel 
+                                    implements ChangeListener,
+                                               ActionListener
 {
 
-   String[] Choices;
-   JTextArea text;
-   JSpinner  spinner;
-   int  selected;
-  
-   public StringListChoiceViewer( String[] Choices, int nrows, int ncols)
+   String[]    Choices;
+
+   boolean     showSelectButton;
+
+   JTextArea   text;
+
+   JSpinner    spinner;
+
+   int         selected;
+
+   JScrollPane scr;
+
+   /**
+    * Constructor
+    * @param Choices  The array of Strings to present
+    * 
+    * @param nrows    Number of rows in display to show the Strings
+    *                 It will be scrolled if necessary
+    *                 
+    * @param ncols    Number of columns in display to show the Strings
+    *                 It will be scrolled if necessary
+    *                 
+    * @param showSelectButton  If true the Select button will be shown
+    */
+   public StringListChoiceViewer(String[] Choices, 
+                                int       nrows, 
+                                int       ncols,
+                                boolean   showSelectButton)
    {
-      super();
-      setLayout( new BorderLayout());
+      super( );
+      
+      setLayout( new BorderLayout( ) );
+      
       this.Choices = Choices;
-      text = new JTextArea( nrows, ncols);
-      spinner = new JSpinner( new SpinnerNumberModel(1,1, Choices.length,1));
+      this.showSelectButton = showSelectButton;
+      text = new JTextArea( nrows , ncols );
+      
+      spinner = new JSpinner( 
+             new SpinnerNumberModel( 1 , 1 , Choices.length , 1 ) );
       spinner.addChangeListener( this );
+      
       text.setText( Choices[0] );
+      text.setFont( gov.anl.ipns.ViewTools.UI.FontUtil.MONO_FONT );
+      
       selected = -1;
-      JButton select = new JButton("Select");
-      select.addActionListener( this);
-      //-------------------------------------
-      JPanel panel = new JPanel();
-      BoxLayout bl = new BoxLayout( panel, BoxLayout.X_AXIS);
+      
+      JButton select = null;
+      if ( showSelectButton )
+      {
+         select = new JButton( "Select" );
+         select.addActionListener( this );
+      }
+      // -------------------------------------
+      JPanel panel = new JPanel( );
+      BoxLayout bl = new BoxLayout( panel , BoxLayout.X_AXIS );
       panel.setLayout( bl );
-      panel.add(  Box.createHorizontalGlue( ) );
-      panel.add(  select );
-      panel.add(  new JLabel("Choice", SwingConstants.RIGHT) );
-      panel.add( spinner);
-      add( panel, BorderLayout.NORTH );
-      //-------------------------------
-      add( new JScrollPane(text), BorderLayout.CENTER );
+      
+      panel.add( Box.createHorizontalGlue( ) );
+      if ( select != null )
+         panel.add( select );
+      
+      panel.add( new JLabel( "Choice" , SwingConstants.RIGHT ) );
+      
+      panel.add( spinner );
+      
+      add( panel , BorderLayout.NORTH );
+      
+      // -------------------------------
+      
+      scr = new JScrollPane( text );
+      add( scr , BorderLayout.CENTER );
    }
+
    
-   public void setNewStringList( String[] Choices)
+   /**
+    * Sets a new List of Strings to cycle through
+    * 
+    * @param Choices  The list of Strings
+    */
+   public void setNewStringList(String[] Choices)
    {
+
       this.Choices = Choices;
-      spinner.setModel( new SpinnerNumberModel(1,1, Choices.length,1));
+      spinner.setModel( new SpinnerNumberModel( 1 , 1 , Choices.length , 1 ) );
       text.setText( Choices[0] );
       selected = -1;
    }
-   
+
+   /**
+    * The last choice from the Select button. It Starts at 0 event
+    * though the spinner starts at 1.
+    * 
+    * @return The last choice from the Select button. Starting at 0.
+    */
    public int getSelectedChoice()
    {
+
       return selected;
    }
-   
+
+   /**
+    * The index-1 of the viewed String. It Starts at 0 event 
+    * though the spinner starts at 1.
+    * 
+    * @return The index of the viewed String. Starting at 0.
+    */
    public int getLastViewedChoice()
    {
-      return ((Number)spinner.getValue()).intValue()-1;
-   }
-   
 
-  
+      return ( ( Number ) spinner.getValue( ) ).intValue( ) - 1;
+   }
+
    @Override
    public void actionPerformed(ActionEvent arg0)
    {
 
-      selected = ((Integer)spinner.getValue()).intValue()-1;
-      
+      selected = ( ( Integer ) spinner.getValue( ) ).intValue( ) - 1;
+
    }
 
    @Override
    public void stateChanged(ChangeEvent arg0)
    {
 
-      int show =((Integer)spinner.getValue()).intValue()-1;
-      text.setText(  Choices[show] );
-      
+      int show = ( ( Integer ) spinner.getValue( ) ).intValue( ) - 1;
+      text.setText( Choices[show] );
+      scr.getViewport( ).setViewPosition( new Point( 0 , 0 ) );
    }
 
    /**
@@ -83,16 +184,19 @@ public class StringListChoiceViewer extends JPanel  implements ChangeListener,
     */
    public static void main(String[] args)
    {
-     String[] choices={"abc","def","ghi","jkl"};
-     StringListChoiceViewer L = new StringListChoiceViewer( choices, 5,12);
-     
-     //JOptionPane.showMessageDialog( null , L );
-     //System.out.println( L.getSelectedChoice()+","+L.getLastViewedChoice());
-     JFrame jf = new JFrame();
-     jf.getContentPane().setLayout(  new GridLayout(1,1) );
-     jf.getContentPane().add(  L );
-     jf.setSize( 300,500);
-     jf.setVisible(  true );
+
+      String[] choices =
+      { "abc" , "def" , "ghi" , "jkl" };
+      StringListChoiceViewer L = new StringListChoiceViewer( choices , 5 , 12 ,
+            false );
+
+      // JOptionPane.showMessageDialog( null , L );
+      // System.out.println( L.getSelectedChoice()+","+L.getLastViewedChoice());
+      JFrame jf = new JFrame( );
+      jf.getContentPane( ).setLayout( new GridLayout( 1 , 1 ) );
+      jf.getContentPane( ).add( L );
+      jf.setSize( 300 , 500 );
+      jf.setVisible( true );
 
    }
 
