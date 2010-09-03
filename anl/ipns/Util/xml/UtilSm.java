9fe52659b08bb35ad7dca7d1a44c8cb9456cdfa7
Ruth Mikkelson <mikkelsonr@uwstout.edu>
2010-09-03 18:09:08 +0000

    Added GPL

--- anl/ipns/Util/xml/UtilSm.java
+++ anl/ipns/Util/xml/UtilSm.java
@@ -1,3 +1,36 @@
+/* 
+ * File: UtilSm.java
+ *
+ * Copyright (C) 2010, Ruth Mikkelson
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * Contact : Ruth Mikkelson <mikkelsonr@uwstout.edu>
+ *           Department of Mathematics, Statistics and Computer Science
+ *           University of Wisconsin-Stout
+ *           Menomonie, WI 54751, USA
+ *
+ * This work was supported by the Spallation Neutron Source Division
+ * of Oak Ridge National Laboratory, Oak Ridge, TN, USA.
+ *
+ *  Last Modified:
+ * 
+ *  $Author:$
+ *  $Date:$            
+ *  $Rev:$
+ */
 package gov.anl.ipns.Util.xml;
 
 
