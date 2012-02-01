bf9254fbafcb21dac733820cc21cceb62c85c796
Dennis Mikkelson <mikkelsond@uwstout.edu>
2012-02-01 17:14:46 +0000

    Update copyright date (test commit to svn)

--- anl/ipns/MathTools/Geometry/Vector3D.java
+++ anl/ipns/MathTools/Geometry/Vector3D.java
@@ -1,7 +1,7 @@
 /*
  * File:  Vector3D.java
  *
- * Copyright (C) 2001, Dennis Mikkelson
+ * Copyright (C) 2001-2011, Dennis Mikkelson
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
